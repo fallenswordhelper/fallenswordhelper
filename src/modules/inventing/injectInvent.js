9e0d10df88887a7eac42674d20ab76363efe8df0
ahanden <handena@pitt.edu>
2021-02-01 15:25:37 -0500

    Fixed amiguous regular expression

--- src/modules/inventing/injectInvent.js
+++ src/modules/inventing/injectInvent.js
@@ -24,7 +24,7 @@ let invResults;
 function parseIngredientTable(table) {
   const [have, need] = getText(querySelector('tr:nth-child(2) td', table)).split('/').map((v) => parseInt(v, 10));
   return Object({
-    id: querySelector('img', table).src.match(/(\d+)\.[A-z]+$/)[1],
+    id: querySelector('img', table).src.match(/(\d+)\.[A-Za-z]+$/)[1],
     have,
     need,
   });
