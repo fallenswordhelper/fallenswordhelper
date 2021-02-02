7402ad8da28c35e9b39f06d8b0c57b8f9a10ee59
ahanden <handena@pitt.edu>
2021-02-02 09:31:43 -0500

    Reorganized code to meet codebeat checks.

--- src/modules/world/newMap/champAttacks.js
+++ src/modules/world/newMap/champAttacks.js
@@ -2,16 +2,14 @@ import getElementsByTagName from '../../common/getElementsByTagName';
 import on from '../../common/on';
 import querySelectorArray from '../../common/querySelectorArray';
 
+let isNineDown = false;
+
 function toggleAttackImages(showCommon) {
   const commonAttacks = querySelectorArray(
-    '#actionList '
-    + '.actionListItem.creature.creature-0 '
-    + '.verb.attack',
+    '#actionList .actionListItem.creature.creature-0 .verb.attack',
   );
   const uncommonAttacks = querySelectorArray(
-    '#actionList '
-    + '.actionListItem.creature:not(.creature-0) '
-    + '.verb.attack',
+    '#actionList .actionListItem.creature:not(.creature-0) .verb.attack',
   );
 
   const [g1, g2] = showCommon
@@ -21,45 +19,47 @@ function toggleAttackImages(showCommon) {
   const blankURL = 'url("https://cdn2.fallensword.com/ui/world/icon_action_attack.png")';
   const numberedURLPrefix = 'url("https://cdn2.fallensword.com/ui/world/icon_action_attack_';
 
-  g1.forEach((e) => {
-    e.style.backgroundImage = blankURL;
-  });
-  g2.forEach((e, i) => {
-    if (i < 8) {
-      e.style.backgroundImage = `${numberedURLPrefix}${i + 1}.png")`;
-    }
+  g1.forEach((e) => { e.style.backgroundImage = blankURL; });
+  g2.filter((e, i) => i < 8).forEach((e, i) => {
+    e.style.backgroundImage = `${numberedURLPrefix}${i + 1}.png")`;
   });
 }
 
+function nineDown(e) {
+  if (e.target.tagName !== 'INPUT' && e.target.tagName !== 'TEXTAREA' && e.key === '9') {
+    toggleAttackImages(false);
+    isNineDown = true;
+  }
+}
+
+function nineUp(e) {
+  if (e.target.tagName !== 'INPUT' && e.target.tagName !== 'TEXTAREA' && e.key === '9') {
+    toggleAttackImages(true);
+    isNineDown = false;
+  }
+}
+
+function nineListeners() {
+  on(document, 'keydown', nineDown);
+  on(document, 'keyup', nineUp);
+}
+
+function attackChamp(e) {
+  if (!isNineDown || !e.key.match(/[1-8]/)) { return; }
+  e.stopPropagation();
+  const uncommonAttacks = querySelectorArray(
+    '#actionList '
+    + '.actionListItem.creature:not(.creature-0) '
+    + '.verb.attack',
+  );
+  const index = parseInt(e.key, 10) - 1;
+  if (index < uncommonAttacks.length) {
+    uncommonAttacks[index].click();
+  }
+}
+
 export default function addListeners() {
-  let nineDown = false;
-  on(document, 'keydown', (e) => {
-    if (e.target.tagName !== 'INPUT' && e.target.tagName !== 'TEXTAREA' && e.key === '9') {
-      toggleAttackImages(false);
-      nineDown = true;
-    }
-  });
-
-  on(document, 'keyup', (e) => {
-    if (e.target.tagName !== 'INPUT' && e.target.tagName !== 'TEXTAREA' && e.key === '9') {
-      toggleAttackImages(true);
-      nineDown = false;
-    }
-  });
-
+  nineListeners();
   const body = getElementsByTagName('body')[0];
-  on(body, 'keydown', (e) => {
-    if (nineDown && e.key.match(/[1-8]/)) {
-      e.stopPropagation();
-      const uncommonAttacks = querySelectorArray(
-        '#actionList '
-        + '.actionListItem.creature:not(.creature-0) '
-        + '.verb.attack',
-      );
-      const index = parseInt(e.key, 10) - 1;
-      if (index < uncommonAttacks.length) {
-        uncommonAttacks[index].click();
-      }
-    }
-  });
+  on(body, 'keydown', attackChamp);
 }
