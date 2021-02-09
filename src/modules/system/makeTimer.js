8f51c8de118b30e53a52ce2254cce601110ce203
ahanden <handena@pitt.edu>
2021-02-09 18:33:47 -0500

    Added timer methods by Jake Archibald

---
+++ src/modules/system/makeTimer.js
@@ -0,0 +1,29 @@
+/* Timer functions taken from Jack Archibald
+ * https://gist.github.com/jakearchibald
+ * https://gist.github.com/jakearchibald/cb03f15670817001b1157e62a076fe95 */
+function setTimer(ms, signal, callback) {
+  const start = document.timeline.currentTime;
+
+  function frame(time) {
+    if (signal.aborted) return;
+    callback(time);
+    // eslint-disable-next-line no-use-before-define
+    scheduleFrame(time);
+  }
+
+  function scheduleFrame(time) {
+    const elapsed = time - start;
+    const roundedElapsed = Math.round(elapsed / ms) * ms;
+    const targetNext = start + roundedElapsed + ms;
+    const delay = targetNext - performance.now();
+    setTimeout(() => requestAnimationFrame(frame), delay);
+  }
+
+  scheduleFrame(start);
+}
+export default function makeTimer(ms, fn) {
+  // eslint-disable-next-line no-undef
+  const controller = new AbortController();
+  setTimer(1000, controller.signal, fn);
+  return controller;
+}
