07d4e4276f1bd2f5088ba7cbd18a6b342ef5d1fe
ahanden <handena@pitt.edu>
2021-03-18 22:30:58 -0400

    Removed extraneous file

--- src/modules/arena/arenaSetup/ArenaMove.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { cdn } from '../../system/system';
-
-  export let slotId;
-  export let moveId;
-</script>
-<a href="index.php?cmd=arena&subcmd=pickmove&slot_id={slotId}" style="display: inline-block">
-  <img src="{cdn}arena/{moveId}.png" />
-</a>
