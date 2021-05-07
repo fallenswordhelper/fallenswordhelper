3c2bdbb0f9a3cfc364c0279005b4b68ab04fc282
ahanden <handena@pitt.edu>
2021-05-07 17:14:38 -0400

    Started work on svelte object

---
+++ src/modules/buffmarket/BuyButton.svelte
@@ -0,0 +1,20 @@
+<script>
+	import daBuyBuff from '../_dataAccess/daBuyBuff';
+	import { oldActionSpinner } from '../support/constants';
+
+	export let buffId;
+	let promise;
+
+	function buyBuff() {
+		promise = daBuyBuff(buffId);
+	};
+</script>
+{#await promise}
+<img src={oldActionSpinner} alt="loading" />
+{:then}
+<input
+	value="Buy"
+	type="button"
+	style="margin-bottom: 5px;"
+	on:click={buyBuff} />
+{/await}
