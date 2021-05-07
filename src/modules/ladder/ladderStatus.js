fcbdc0d3591cfea7283215ec5b13faf7a5c5bcf5
ahanden <handena@pitt.edu>
2021-05-07 16:32:02 -0400

    Fixed if-statment for deepscan

--- src/modules/ladder/ladderStatus.js
+++ src/modules/ladder/ladderStatus.js
@@ -2,8 +2,8 @@ import callApp from '../app/callApp';
 
 export default async function ladderStatus() {
   const settings = await callApp({ cmd: 'settings' });
-  if (settings !== undefined || settings.s === true) {
-    return settings.r.flags[0];
+  if (settings === undefined || settings.s === false) {
+    return null;
   }
-  return null;
+  return settings.r.flags[0];
 }
