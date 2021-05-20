0d9f21ca4b85c45a7f96f1d6411be1a621e43f8c
ahanden <handena@pitt.edu>
2021-05-20 07:52:52 -0400

    Removed unused da file

--- src/modules/_dataAccess/daToggleLadder.js
+++
@@ -1,7 +0,0 @@
-import $dataAccess from './_dataAccess';
-import toggle from '../ladder/toggle';
-import toggleLadder from './fallbacks/toggleLadder';
-
-export default function daToggleLadder(ladderStatus) {
-  return $dataAccess(toggle, toggleLadder, ladderStatus);
-}
