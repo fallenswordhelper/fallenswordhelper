8eb0eaea054fda0154a2f4f00a47b9b7f3ef04c3
atompkins <atompkins@gmail.com>
2021-07-05 23:59:55 +0100

    reduce nesting for codebeat

--- src/modules/logs/fixFilters.js
+++ src/modules/logs/fixFilters.js
@@ -5,34 +5,40 @@ import on from '../common/on';
 import onclick from '../common/onclick';
 import { pCC } from '../support/layout';
 
+let thisType;
+let injectType;
+let prevPage;
+let nextPage;
+
+const strategies = [
+  ['All', () => ''],
+  ['<', () => `${prevPage}${injectType}`],
+  ['>', () => `${nextPage}${injectType}`],
+];
+
+function clickHandler(e) {
+  if (e.target.tagName !== 'INPUT') { return; }
+  const thisStrategy = strategies.find(([label]) => label === e.target.value);
+  if (thisStrategy) {
+    e.stopPropagation();
+    navigateTo(`${indexPhp}?cmd=log${thisStrategy[1]()}`);
+  }
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+  const myParams = new URLSearchParams(new FormData(e.target));
+  const targetType = myParams.get('type');
+  if (targetType !== thisType) { myParams.delete('type'); }
+  navigateTo(`${indexPhp}?${myParams.toString()}`);
+}
+
 export default function fixFilters() {
-  const thisType = getUrlParameter('type');
-  const injectType = thisType ? `&type=${thisType}` : '';
-
+  thisType = getUrlParameter('type');
+  injectType = thisType ? `&type=${thisType}` : '';
   const pageNo = getUrlParameter('page');
-  const prevPage = pageNo ? `&page=${Number(pageNo) - 1}` : '';
-  const nextPage = pageNo ? `&page=${Number(pageNo) + 1}` : '&page=2';
-
-  on(pCC, 'submit', (e) => {
-    e.preventDefault();
-    const myParams = new URLSearchParams(new FormData(e.target));
-    const targetType = myParams.get('type');
-    if (targetType !== thisType) { myParams.delete('type'); }
-    navigateTo(`${indexPhp}?${myParams.toString()}`);
-  });
-
-  onclick(pCC, (e) => {
-    if (e.target.tagName === 'INPUT' && ['All', '<', '>'].includes(e.target.value)) {
-      e.stopPropagation();
-      if (e.target.value === 'All') {
-        navigateTo(`${indexPhp}?cmd=log`);
-      }
-      if (e.target.value === '<') {
-        navigateTo(`${indexPhp}?cmd=log${prevPage}${injectType}`);
-      }
-      if (e.target.value === '>') {
-        navigateTo(`${indexPhp}?cmd=log${nextPage}${injectType}`);
-      }
-    }
-  }, true);
+  prevPage = pageNo ? `&page=${Number(pageNo) - 1}` : '';
+  nextPage = pageNo ? `&page=${Number(pageNo) + 1}` : '&page=2';
+  on(pCC, 'submit', submitHandler);
+  onclick(pCC, clickHandler, true);
 }
