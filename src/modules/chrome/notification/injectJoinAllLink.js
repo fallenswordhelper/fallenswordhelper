d0e6082c142ba73b118be35f53ca934bde4e1e47
atompkins <atompkins@gmail.com>
2021-09-13 16:22:29 +0100

    join all ajax

--- src/modules/chrome/notification/injectJoinAllLink.js
+++ src/modules/chrome/notification/injectJoinAllLink.js
@@ -1,26 +1,58 @@
 import calf from '../../support/calf';
+import closest from '../../common/closest';
+import contains from '../../common/contains';
+import createLi from '../../common/cElement/createLi';
+import delay from '../../_dataAccess/delay';
 import getArrayByTagName from '../../common/getArrayByTagName';
-import getText from '../../common/getText';
-import insertHtmlAfterEnd from '../../common/insertHtmlAfterEnd';
+import getElementById from '../../common/getElementById';
+import indexAjaxData from '../../ajax/indexAjaxData';
+import insertElementAfter from '../../common/insertElementAfter';
+import insertElementAfterBegin from '../../common/insertElementAfterBegin';
+import once from '../../common/once';
 import { pCL } from '../../support/layout';
+import setInnerHtml from '../../dom/setInnerHtml';
 import { joinUnderUrl, joinallUrl } from '../../support/constants';
 
-function findNewGroup(el) {
-  if (!getText(el).includes('New attack group created.')) { return; }
-  let groupJoinHTML = '';
-  if (!calf.enableMaxGroupSizeToJoin) {
-    groupJoinHTML = `<a href="${joinallUrl}"><span `
-      + 'class="notification-icon"></span><p class="notification-content">'
-      + 'Join all attack groups.</p></a>';
+function buildHandler(newAttackGroup) {
+  return async function handler(e) {
+    // console.log(e);
+    e.preventDefault();
+    const li = closest('li', e.target);
+    setInnerHtml('<span class="notification-icon"></span>'
+      + '<span class="notification-content fshSpinner fshRelative"></span>', li);
+    if (newAttackGroup) {
+      await indexAjaxData({ cmd: 'guild', subcmd: 'groups', subcmd2: 'joinall' });
+    } else {
+      await delay(1000);
+    }
+    setInnerHtml(
+      '<span class="notification-icon"></span>'
+        + '<p class="notification-content" style="line-height: 32px;">Joined.</p>',
+      li,
+    );
+  };
+}
+
+function joinAllLink(newAttackGroup) {
+  const groupJoinUrl = calf.enableMaxGroupSizeToJoin ? joinUnderUrl : joinallUrl;
+  const groupJoinText = calf.enableMaxGroupSizeToJoin ? ` less than size ${
+    calf.maxGroupSizeToJoin}` : '';
+  const notification = createLi({
+    className: 'notification',
+    innerHTML: `<a href="${groupJoinUrl}"><span class="notification-icon"></span>`
+      + `<p class="notification-content">Join all attack groups${groupJoinText}.</p></a>`,
+  });
+  once(notification, 'click', buildHandler(newAttackGroup), true);
+  if (newAttackGroup) {
+    insertElementAfter(notification, newAttackGroup);
   } else {
-    groupJoinHTML = `<a href="${joinUnderUrl}"><span `
-      + 'class="notification-icon"></span><p class="notification-content">'
-      + `Join all attack groups less than size ${calf.maxGroupSizeToJoin
-      }.</p></a>`;
+    insertElementAfterBegin(getElementById('notifications'), notification);
   }
-  insertHtmlAfterEnd(el, `<li class="notification">${groupJoinHTML}</li>`);
 }
 
 export default function injectJoinAllLink() {
-  getArrayByTagName('li', pCL).forEach(findNewGroup);
+  const newAttackGroup = getArrayByTagName('li', pCL)
+    .find(contains('New attack group created.'));
+  if (newAttackGroup) { joinAllLink(newAttackGroup); }
+  if (calf.userIsDev && !newAttackGroup) { joinAllLink(); }
 }
