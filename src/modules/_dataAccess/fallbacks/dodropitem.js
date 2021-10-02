abd3207181b8338a5654f3d7c9b3b8a072842dd3
atompkins <atompkins@gmail.com>
2021-10-02 23:16:12 +0100

    typo

--- src/modules/_dataAccess/fallbacks/dodropitem.js
+++ src/modules/_dataAccess/fallbacks/dodropitem.js
@@ -4,7 +4,7 @@ export default async function dodropitem(itemsAry) {
   const response = await dropItem(itemsAry);
   const appResponse = { s: response.r === 0 };
   if (appResponse.s) {
-    appResponse.r = { items: [], deleted_itesm: itemsAry };
+    appResponse.r = { items: [], deleted_items: itemsAry };
   } else {
     appResponse.e = { message: response.m };
   }
