8e99a715aa0c33b46bcc5d596bbb99c151a41a19
atompkins <atompkins@gmail.com>
2021-10-02 19:31:58 +0100

    reduce ABC codebeat

--- src/modules/_dataAccess/fallbacks/profileLimits.js
+++ src/modules/_dataAccess/fallbacks/profileLimits.js
@@ -6,23 +6,20 @@ import retryAjax from '../../ajax/retryAjax';
 const lastHref = (anchors) => anchors[anchors.length - 1].href;
 const getSlots = (doc) => querySelectorArray('td[background*="1x1mini"]', doc).length;
 
+async function calcMax(profileDoc, anchors) {
+  if (anchors.length > 1) {
+    const lastDoc = createDocument(await retryAjax(lastHref(anchors)));
+    return ((anchors.length - 1) * 50) + getSlots(lastDoc);
+  }
+  return getSlots(profileDoc);
+}
+
 // Incomplete
 export default async function profileLimits() {
-  let maxComponents = 0;
-  const html = await indexAjaxData({ cmd: 'profile' });
-  const doc = createDocument(html);
-  const anchors = querySelectorArray('a[href*="=profile&component_page="]', doc);
-  let lastDoc;
-  if (anchors.length > 1) {
-    maxComponents = (anchors.length - 1) * 50;
-    const lastHtml = await retryAjax(lastHref(anchors));
-    lastDoc = createDocument(lastHtml);
-  } else {
-    lastDoc = doc;
-  }
-  maxComponents += getSlots(lastDoc);
+  const profileDoc = createDocument(await indexAjaxData({ cmd: 'profile' }));
+  const anchors = querySelectorArray('a[href*="=profile&component_page="]', profileDoc);
   return {
-    r: { max_components: maxComponents },
+    r: { max_components: await calcMax(profileDoc, anchors) },
     s: true,
   };
 }
