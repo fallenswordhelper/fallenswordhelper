32c77f03ee3371bd4a004940f0eda70572bbb870
ahanden <adam.handen@gmail.com>
2021-12-09 02:58:29 -0500

    Reworked UX

--- src/modules/settings/BlockedSkills.svelte
+++ src/modules/settings/BlockedSkills.svelte
@@ -1,103 +1,206 @@
 <script>
-import arrayFrom from '../common/arrayFrom';
-import getValue from '../system/getValue';
+import getValueJSON from '../system/getValueJSON';
+import isUndefined from '../common/isUndefined';
+import querySelector from '../common/querySelector';
+import querySelectorAll from '../common/querySelectorAll';
+import querySelectorArray from '../common/querySelectorArray';
 import setValue from '../system/setValue';
 
-let blockedSkillLists = JSON.parse(getValue('blockedSkillLists'));
-let listName = '-1';
-let applied = false;
+let blockedSkillLists = getValueJSON('blockedSkillLists');
+let newListName = '';
 let error = false;
+let listName = '';
 
-function getBlockedSkills() {
-  const inputs = document.querySelectorAll('input[name="blockedSkillList[]"]');
-  return arrayFrom(inputs).filter((i) => i.checked).map((i) => i.value);
+function apply() {
+  querySelector('input[value="Save Blocked Skill Changes"]').click();
 }
 
-/* eslint-disable no-param-reassign */
-function applyBlockedSkillList() {
-  const blockedSkillList = blockedSkillLists.find((i) => i.name === listName);
-  if (blockedSkillList === undefined) {
-    error = 'Unable to find this list. Try refreshing the page.';
+function clearMessages() {
+  error = false;
+}
+
+function storeBlockedSkillsLists() {
+  setValue('blockedSkillLists', JSON.stringify(blockedSkillLists));
+}
+
+function getCheckedSkills() {
+  return querySelectorArray('input[name="blockedSkillList[]"]', document)
+    .filter((i) => i.checked)
+    .map((i) => i.value);
+}
+
+function clearCheckedSkills() {
+  const inputs = querySelectorAll('input[name="blockedSkillList[]"]');
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].checked = false;
+  }
+}
+
+function applyList() {
+  clearMessages();
+
+  if (!listName) {
     return;
   }
-  const blockedSkills = blockedSkillList.skills;
-  arrayFrom(document.querySelectorAll('input[name="blockedSkillList[]"]'))
-    .forEach((i) => { i.checked = false; });
-  blockedSkills.forEach((s) => {
-    document.querySelector(`input[name="blockedSkillList[]"][value="${s}"]`).checked = true;
+
+  const skillList = blockedSkillLists.find((l) => l.name === listName);
+  if (!skillList) {
+    error = `Unable to find list "${listName}". Try refreshing the page.`;
+    return;
+  }
+
+  clearCheckedSkills();
+  skillList.skills.forEach((skill) => {
+    querySelector(`input[name="blockedSkillList[]"][value="${skill}"]`)
+      .checked = true;
   });
-  applied = true;
-  error = false;
+  apply();
 }
-/* eslint-enable no-param-reassign */
 
-/* eslint-disable no-alert */
-function saveBlockedSkillList() {
-  const skills = getBlockedSkills();
-  if (listName === '-1') {
-    const rawName = window.prompt('Name your new blocked skill set');
-    if (rawName === undefined) { return; }
-    const name = rawName.trim();
-    if (name.length === 0 || name.length > 50) {
-      error = 'Blocked skill set names must be between 1 and 50 characters.';
-      return;
+function checkForDuplicates(list) {
+  return blockedSkillLists.some((l) => {
+    if (l.name === list.name) {
+      return false;
     }
-    if (blockedSkillLists.find((i) => i.name === name) !== undefined) {
-      error = `You already have a blocked skill list named ${name}`;
-      return;
+    if (l.skills.length !== list.skills.length) {
+      return false;
     }
-    error = false;
-    blockedSkillLists = [...blockedSkillLists, {
-      name,
-      skills,
-    }];
-    listName = name;
-  } else {
-    const index = blockedSkillLists.find((l) => l.name === listName);
-    if (index === undefined) {
-      error = 'Unable to find this list. Try refreshing the page.';
-      return;
+    for (let i = 0; i < l.skills.length; i++) {
+      if (l.skills[i] !== list.skills[i]) {
+        return false;
+      }
     }
-    blockedSkillLists[index].skills = skills;
+    return true;
+  });
+}
+
+function deleteList() {
+  clearMessages();
+  blockedSkillLists = blockedSkillLists.filter((l) => l.name !== listName);
+  storeBlockedSkillsLists();
+  listName = '-1';
+}
+
+function createList() {
+  clearMessages();
+
+  const cleanListName = newListName.trim();
+  if (!cleanListName.length) {
+    error = 'You must name your list.';
+    return;
   }
-  setValue('blockedSkillLists', JSON.stringify(blockedSkillLists));
+  if (cleanListName.length > 50) {
+    error = 'List names can only be at most 50 characters long.';
+    return;
+  }
+  if (blockedSkillLists.some((l) => l.name === cleanListName)) {
+    error = `You already have a list named ${cleanListName}.`;
+    return;
+  }
+
+  const newList = {
+    name: cleanListName,
+    skills: getCheckedSkills(),
+  };
+
+  if (checkForDuplicates(newList)) {
+    error = 'You already have a list with these skills.';
+    return;
+  }
+
+  blockedSkillLists = [...blockedSkillLists, newList];
+  listName = newList.name;
+  newListName = '';
+  storeBlockedSkillsLists();
+
+  apply();
+}
+
+function updateList() {
+  clearMessages();
+  const index = blockedSkillLists.findIndex((l) => l.name === listName);
+  if (isUndefined(index)) {
+    error = `Cannot find list named "${listName}". Try refreshing the page.`;
+    return;
+  }
+
+  const skills = getCheckedSkills();
+  const skillList = { name: listName, skills };
+
+  if (checkForDuplicates(skillList)) {
+    error = 'You already have a list with these skills.';
+    return;
+  }
+
+  blockedSkillLists[index] = skillList;
+  storeBlockedSkillsLists();
+  apply();
+}
+
+function findLoadList() {
+  const skills = getCheckedSkills();
+  const list = blockedSkillLists.find((l) => {
+    if (l.skills.length !== skills.length) {
+      return false;
+    }
+    for (let i = 0; i < l.skills.length; i++) {
+      if (l.skills[i] !== skills[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  if (list) {
+    listName = list.name;
+  }
+
   if (blockedSkillLists.length >= 10) {
-    error = 'Having more than 10 blocked skill sets may slow down this page.';
-  } else {
-    error = false;
+    error = 'Having more than 10 blocked skills lists may slow down this page.';
   }
 }
-/* eslint-enable no-alert */
 
-/* eslint-disable no-alert */
-function deleteBlockedSkillList() {
-  if (listName === '-1') { return; }
-  if (window.confirm('Are you sure you want to delete this set?')) {
-    blockedSkillLists = blockedSkillLists.filter((l) => l.name !== listName);
-    listName = '-1';
-  }
-  setValue('blockedSkillLists', JSON.stringify(blockedSkillLists));
-  error = false;
-}
-/* eslint-enable no-alert */
+findLoadList();
 </script>
-<div style='text-align: center;'>
+<div style='text-align: center; margin-top: 12px'>
+  <div>
     <select id='fsh-skillSets' bind:value={listName}>
-        <option value='-1'>New Block List</option>
-        {#each blockedSkillLists as bsl}
+        {#each blockedSkillLists as bsl (bsl.name)}
         <option value={bsl.name}>{bsl.name}</option>
         {/each}
     </select>
-    <input class='custominput' type='button' value='Use' on:click|self={applyBlockedSkillList} />
-    <input class='custominput' type='button' value='Delete' on:click|self={deleteBlockedSkillList} />
-    <input class='custominput' type='button' value='Save' on:click|self={saveBlockedSkillList} />
-    {#if applied}
-    <p style='padding-top: 12px;'>Make sure to click the 'Save Blocked Skill Changes' to apply your list.</p>
-    {/if}
-    {#if error !== false}
-    <div style="border: 2px solid rgb(255, 255, 255); margin: 10px auto; width: 80%; background: rgb(211, 207, 193);">
-      <div style="background: rgb(142, 134, 104); color: rgb(255, 255, 255); font-size: smaller;">INFORMATION</div>
+    <input class='custominput'
+      type='button'
+      value='Use'
+      on:click|self={applyList} />
+    <input class='custominput'
+      type='button'
+      value='Delete'
+      on:click|self={deleteList} />
+    <input
+      class='custominput'
+      type='button'
+      value='Update'
+      on:click|self={updateList} />
+  </div>
+  <div style="margin-top: 6px">
+    <input
+      class='custominput'
+      type='text'
+      name='new-set-name'
+      id='new-set-name'
+      placeholder='Set Name'
+      maxlength='50'
+      bind:value={newListName} />
+    <input
+      class='custominput'
+      type='button'
+      value='Save New Blocked Skill Set'
+      on:click|self={createList}/>
+  </div>
+  {#if error}
+  <div style="border: 2px solid rgb(255, 255, 255); margin: 10px auto; width: 80%; background: rgb(211, 207, 193);">
+    <div style="background: rgb(142, 134, 104); color: rgb(255, 255, 255); font-size: smaller;">INFORMATION</div>
       <div>{error}</div>
     </div>
-    {/if}
+  {/if}
 </div>
