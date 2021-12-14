89d30650282b1c651a84d3595368986e208a4ea0
atompkins <atompkins@gmail.com>
2021-12-14 16:37:21 +0000

    app api first pass

--- src/modules/settings/blockedSkills.js
+++ src/modules/settings/blockedSkills.js
@@ -1,6 +1,7 @@
 import querySelector from '../common/querySelector';
 import querySelectorAll from '../common/querySelectorAll';
 import querySelectorArray from '../common/querySelectorArray';
+import skill from '../app/settings/skill';
 
 export function checkSkill(skillId) {
   querySelector(`input[name="blockedSkillList[]"][value="${skillId}"]`)
@@ -21,5 +22,16 @@ export function clearCheckedSkills() {
 }
 
 export function submitSkillChanges() {
-  querySelector('input[value="Save Blocked Skill Changes"]').click();
+  const checkboxes = querySelectorAll('input[name="blockedSkillList[]"]');
+  const level = [];
+  const blocked = [];
+  for (const i of checkboxes) {
+    level[i.value] = 0;
+    blocked[i.value] = i.checked ? 1 : 0;
+  }
+  level[54] = Number(querySelector('input[name="ca_default"]').value);
+  level[101] = Number(querySelector('input[name="sc_default"]').value);
+  level[60] = Number(querySelector('input[name="nv_default"]').value);
+  level[98] = Number(querySelector('input[name="barricade_default"]').value);
+  skill(level, blocked);
 }
