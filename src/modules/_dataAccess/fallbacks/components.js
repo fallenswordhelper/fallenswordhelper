a5fefc3cc3d1233fb0cc87ffadf2c8f905e4959b
atompkins <atompkins@gmail.com>
2022-01-29 19:18:24 +0000

    fix components total

--- src/modules/_dataAccess/fallbacks/components.js
+++ src/modules/_dataAccess/fallbacks/components.js
@@ -1,8 +1,10 @@
 import all from '../../common/all';
 import createDocument from '../../system/createDocument';
 import indexAjaxData from '../../ajax/indexAjaxData';
+import querySelectorAll from '../../common/querySelectorAll';
 import querySelectorArray from '../../common/querySelectorArray';
 import retryAjax from '../../ajax/retryAjax';
+import sum from '../../common/sum';
 
 const componentRe = /\?item_id=(\d+)&inv_id=(\d+)&.*&vcode=([0-9a-f]+)/;
 
@@ -16,6 +18,13 @@ function details(a) {
 }
 
 const getComponents = (doc) => querySelectorArray('a[href*="=destroycomponent&"]', doc).map(details);
+const componentSlots = (doc) => querySelectorAll('td[background*="/1x1mini."]', doc).length;
+
+function fakeHud(asDocs) {
+  const p = Array(57);
+  p[56] = { k: 56, v: asDocs.map(componentSlots).reduce(sum, 0) };
+  return { p };
+}
 
 // Incomplete
 export default async function components() {
@@ -24,5 +33,5 @@ export default async function components() {
   const pages = querySelectorArray('a[href*="profile&component_page="]', profileDoc);
   const profiles = await all([profileHtml, ...pages.slice(1).map((a) => retryAjax(a.href))]);
   const asDocs = profiles.map(createDocument);
-  return { r: asDocs.flatMap(getComponents), s: true };
+  return { h: fakeHud(asDocs), r: asDocs.flatMap(getComponents), s: true };
 }
