b446ad54204889844f94563802b573da9f58c340
ahanden <adam.handen@gmail.com>
2022-02-15 17:13:08 -0500

    Fixed fallback imports

--- src/modules/_dataAccess/daBankDeposit.js
+++ src/modules/_dataAccess/daBankDeposit.js
@@ -1,6 +1,6 @@
 import $dataAccess from './$dataAccess';
 import bank from '../app/bank';
-import { fallback } from './fallbacks/bank';
+import fallback from './fallbacks/bank';
 
 export default function daBankDeposit(amount) {
   return $dataAccess(bank, fallback, 'deposit', amount);
--- src/modules/_dataAccess/daBankWithdraw.js
+++ src/modules/_dataAccess/daBankWithdraw.js
@@ -1,6 +1,6 @@
 import $dataAccess from './$dataAccess';
 import bank from '../app/bank';
-import { fallback } from './fallbacks/bank';
+import fallback from './fallbacks/bank';
 
 export default function daBankWithdraw(amount) {
   return $dataAccess(bank, fallback, 'withdraw', amount);
