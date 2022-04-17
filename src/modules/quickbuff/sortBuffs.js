447c578e7ade5ac8f70befd32f15cf7a1c5d633b
Nicholas <chainmill@gmail.com>
2022-04-17 10:04:20 -0500

    Use common functions for querySelector, refactor default buff sort function

--- src/modules/quickbuff/sortBuffs.js
+++ src/modules/quickbuff/sortBuffs.js
@@ -2,18 +2,27 @@ import getElementById from '../common/getElementById';
 import getValue from '../system/getValue';
 import insertHtmlAfterEnd from '../common/insertHtmlAfterEnd';
 import on from '../common/on';
+import querySelector from '../common/querySelector';
+import querySelectorAll from '../common/querySelectorAll';
+import querySelectorArray from '../common/querySelectorArray';
 import setValue from '../system/setValue';
 import { quickBuffSortCheckbox, quickbuffSortStorageName } from './assets';
 
 const defaultBuffOrder = [];
 
 function sortBuffsHelper(parentElementId) {
-  const buffs = document.querySelectorAll(`#${parentElementId} > p`);
-  const buffsArray = Array.prototype.slice.call(buffs, 0);
-  buffsArray.sort((a, b) => (a.children[0].dataset.name > b.children[0].dataset.name ? 1 : -1));
-  const buffBlock = document.querySelector(`#${parentElementId}`);
+  const buffs = querySelectorArray(`#${parentElementId} > p`);
+  buffs.sort((a, b) => (a.children[0].dataset.name > b.children[0].dataset.name ? 1 : -1));
+  const buffBlock = querySelector(`#${parentElementId}`);
   for (let i = 0; i < buffs.length; i++) {
-    buffBlock.appendChild(buffsArray[i]);
+    buffBlock.appendChild(buffs[i]);
+  }
+}
+
+function sortBuffsByDefaultOrder(blockId) {
+  const buffBlock = querySelector(`#${blockId}`);
+  for (let i = 0; i < defaultBuffOrder[blockId].length; i++) {
+    buffBlock.appendChild(defaultBuffOrder[blockId][i]);
   }
 }
 
@@ -23,31 +32,29 @@ function sortBuffs(alphabetically) {
       sortBuffsHelper(`block${i}`);
     } else {
       const blockId = `block${i}`;
-      const buffBlock = document.querySelector(`#${blockId}`);
-      for (let j = 0; j < defaultBuffOrder[blockId].length; j++) {
-        buffBlock.appendChild(defaultBuffOrder[blockId][j]);
-      }
+      sortBuffsByDefaultOrder(blockId);
     }
   }
 }
 
-function onSortBuffsCheckboxChanged() {
-  const checkbox = getElementById('quickbuffSortCheckbox');
-  setValue(quickbuffSortStorageName, checkbox.checked);
-  sortBuffs(checkbox.checked);
+function onSortBuffsCheckboxChanged(event) {
+  setValue(quickbuffSortStorageName, event.target.checked);
+  sortBuffs(event.target.checked);
 }
 
 export default function quickbuffSort() {
   // store the default order of buffs
   for (let i = 1; i <= 3; i++) {
     const block = `block${i}`;
-    defaultBuffOrder[block] = document.querySelectorAll(`#${block} > p`);
+    defaultBuffOrder[block] = querySelectorAll(`#${block} > p`);
   }
   const quickbuffCheckAllCheckbox = getElementById('check-all');
   insertHtmlAfterEnd(quickbuffCheckAllCheckbox, quickBuffSortCheckbox);
   const quickbuffSortCheckbox = getElementById('quickbuffSortCheckbox');
-  const qbSortPref = getValue('qbSortBuffsAlphabetically');
+  const qbSortPref = getValue(quickbuffSortStorageName);
   quickbuffSortCheckbox.checked = qbSortPref;
-  sortBuffs(qbSortPref);
+  if (qbSortPref) {
+    sortBuffs(qbSortPref);
+  }
   on(quickbuffSortCheckbox, 'change', onSortBuffsCheckboxChanged);
 }
