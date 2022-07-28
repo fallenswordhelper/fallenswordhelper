772b347872c1f3084b91ae764ad675759d0e5bf6
atompkins <atompkins@gmail.com>
2022-07-28 01:21:01 +0100

    quiet deepsource

--- src/modules/analytics/sendException.js
+++ src/modules/analytics/sendException.js
@@ -4,7 +4,7 @@ import noGa from './noGa';
 export default function sendException(desc, fatal) {
   if (getValue('betaOptIn')) { //  sendException
     // eslint-disable-next-line no-console
-    console.log('sendException', desc);
+    console.log('sendException', desc); // skipcq: JS-0002
   }
   if (noGa()) { return; }
   ga('fshApp.send', 'exception', {
