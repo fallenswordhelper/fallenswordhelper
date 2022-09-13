5d50acdbe6d110a7b98ab97591ae74bcbb9bb01b
ahanden <adam.handen@gmail.com>
2022-09-13 11:57:01 -0400

    Requested changes

--- src/modules/bazaar/Bazaar.svelte
+++ src/modules/bazaar/Bazaar.svelte
@@ -1,71 +1,68 @@
 <script>
 import daBazaarBuy from '../_dataAccess/daBazaarBuy';
-import dialogMsg from '../common/dialogMsg';
+import dynamicAlert from '../alert/dynamicAlert';
 import addCommas from '../system/addCommas';
+import { cdn } from '../system/system';
 
 export let potions = [];
 
-/* eslint-disable no-param-reassign */
-potions = potions.map((p) => {
-  p.count = 1;
-  p.promise = Promise.resolve();
-  return p;
-});
-
 async function buyButton(potion) {
+  // eslint-disable-next-line no-param-reassign
   potion.promise = daBazaarBuy(potion.id, potion.count);
   const response = await potion.promise;
   if (response.s) {
-    dialogMsg(`You successfully purchased ${potion.count} "${response.r[0].n}"`);
+    dynamicAlert(`You successfully purchased ${potion.count} "${response.r[0].n}"`);
   } else {
-    dialogMsg(response.e.message);
+    dynamicAlert(response.e.message);
   }
 }
-/* eslint-enable no-param-reassign */
 </script>
 <div id="bazaar">
-    {#each potions as potion (potion.id)}
+  {#each potions as potion (potion.id)}
     <div class="potion">
+      <img
+          class="potion-img tip-dynamic"
+          style="background: url('{cdn}ui/inventory/2x3.png')"
+          src="{potion.img}"
+          alt="Potion"
+          data-tipped="{potion.fetch}">
+      <div class="prices">
+        {addCommas(potion.price)}
         <img
-            class="potion-img tip-dynamic"
-            src="{potion.img}"
-            alt="Potion"
-            data-tipped="{potion.fetch}" />
-        <div class="prices">
-            {addCommas(potion.price)} <img
-                src="https://cdn2.fallensword.com/currency/0.png"
-                data-hasqtip="1"
-                oldtitle="Gold"
-                alt="Gold"
-                title=""
-                aria-describedby="qtip-1" />
-            x <input
-                type="number"
-                bind:value={potion.count}
-                min=1
-                max=100 /><br/>
-            = {addCommas(potion.price * potion.count)} <img
-                src="https://cdn2.fallensword.com/currency/0.png"
-                alt="Gold"
-                data-hasqtip="1"
-                oldtitle="Gold"
-                title=""
-                aria-describedby="qtip-1" />
-        </div>
-        {#await potion.promise}
-        <span class="fshSpinner fshSpinner12"></span>
-        {:then}
-        <button type="button" class="custombutton" on:click={buyButton(potion)}>Buy</button>
-        {/await}
+            src="{cdn}currency/0.png"
+            data-hasqtip="1"
+            oldtitle="Gold"
+            alt="Gold"
+            title=""
+            aria-describedby="qtip-1">
+         x
+         <input
+            type="number"
+            bind:value={potion.count}
+            min=1
+            max=100><br>
+         = {addCommas(potion.price * potion.count)}
+         <img
+            src="{cdn}currency/0.png"
+            alt="Gold"
+            data-hasqtip="1"
+            oldtitle="Gold"
+            title=""
+            aria-describedby="qtip-1">
+      </div>
+      {#await potion.promise}
+      <span class="fshSpinner fshSpinner12"></span>
+      {:then}
+      <button type="button" class="custombutton" on:click={buyButton(potion)}>Buy</button>
+      {/await}
     </div>
-    {/each}
+  {/each}
 </div>
 <style>
 #bazaar {
     text-align: center;
 }
 .potion-img {
-   background: url('https://cdn2.fallensword.com/ui/inventory/2x3.png');
    width: 60px;
    height: 90px;
    margin: 0px auto;
--- src/modules/bazaar/bazaar.js
+++ src/modules/bazaar/bazaar.js
@@ -1,5 +1,7 @@
 import closestTable from '../common/closestTable';
 import getId from '../common/getId';
+import getTextTrim from '../common/getTextTrim';
+import querySelector from '../common/querySelector';
 import querySelectorArray from '../common/querySelectorArray';
 import intValue from '../system/intValue';
 import Bazaar from './Bazaar.svelte';
@@ -7,25 +9,24 @@ import Bazaar from './Bazaar.svelte';
 function startApp(potions, target) {
   return new Bazaar({
     target: target.parentElement,
-    anchor: target,
     props: { potions },
   });
 }
 
 export default function bazaar() {
   const origPots = querySelectorArray('a[href*="cmd=buyitem"]');
-  const potTable = origPots[0].closest('table')
-    .parentElement
-    .parentElement
-    .parentElement
-    .parentElement;
+  const potTable = closestTable(closestTable(origPots[0]).parentElement);
   const potions = origPots.map((p) => ({
     id: Number(getId(p)),
-    img: p.firstChild.src,
+    img: p.children[0].src,
     fetch: p.firstChild.dataset.tipped,
-    price: intValue(closestTable(p).parentNode
-      .querySelector('img[oldtitle=Gold]')
-      .closest('table').querySelector('td').textContent),
+    price: intValue(getTextTrim(
+      querySelector('img[src*="/currency/"]', closestTable(p).parentElement)
+        .parentElement
+        .previousElementSibling,
+    )),
+    count: 1,
+    promise: Promise.resolve(),
   }));
 
   startApp(potions, potTable);
