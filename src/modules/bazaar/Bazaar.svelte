0343c70f15c6f5471490a4d2c6146d2c9e8a9903
atompkins <atompkins@users.noreply.github.com>
2022-09-17 18:14:18 +0100

    fix-whitespace

--- src/modules/bazaar/Bazaar.svelte
+++ src/modules/bazaar/Bazaar.svelte
@@ -21,66 +21,70 @@ async function buyButton(potion) {
   {#each potions as potion (potion.id)}
     <div class="potion">
       <img
-          class="potion-img tip-dynamic"
-          style="background: url('{cdn}ui/inventory/2x3.png')"
-          src="{potion.img}"
-          alt="Potion"
-          data-tipped="{potion.fetch}">
+        class="potion-img tip-dynamic"
+        style="background: url('{cdn}ui/inventory/2x3.png')"
+        src="{potion.img}"
+        alt="Potion"
+        data-tipped="{potion.fetch}"
+      >
       <div class="prices">
         {addCommas(potion.price)}
         <img
-            src="{cdn}currency/0.png"
-            data-hasqtip="1"
-            oldtitle="Gold"
-            alt="Gold"
-            title=""
-            aria-describedby="qtip-1">
-         x
-         <input
-            type="number"
-            bind:value={potion.count}
-            min=1
-            max=100><br>
-         = {addCommas(potion.price * potion.count)}
-         <img
-            src="{cdn}currency/0.png"
-            alt="Gold"
-            data-hasqtip="1"
-            oldtitle="Gold"
-            title=""
-            aria-describedby="qtip-1">
+          src="{cdn}currency/0.png"
+          data-hasqtip="1"
+          oldtitle="Gold"
+          alt="Gold"
+          title=""
+          aria-describedby="qtip-1"
+        >
+        x
+        <input
+          type="number"
+          bind:value={potion.count}
+          min=1
+          max=100
+        ><br>
+        = {addCommas(potion.price * potion.count)}
+        <img
+          src="{cdn}currency/0.png"
+          alt="Gold"
+          data-hasqtip="1"
+          oldtitle="Gold"
+          title=""
+          aria-describedby="qtip-1"
+        >
       </div>
       {#await potion.promise}
-      <span class="fshSpinner fshSpinner12"></span>
+        <span class="fshSpinner fshSpinner12"></span>
       {:then}
-      <button type="button" class="custombutton" on:click={buyButton(potion)}>Buy</button>
+        <button type="button" class="custombutton" on:click={buyButton(potion)}>Buy</button>
       {/await}
     </div>
   {/each}
 </div>
 <style>
 #bazaar {
-    text-align: center;
+  text-align: center;
 }
 .potion-img {
-   width: 60px;
-   height: 90px;
-   margin: 0px auto;
+  width: 60px;
+  height: 90px;
+  margin: 0px auto;
 }
 .potion {
-    display: inline-block;
-    width: 120px;
-    text-align: center;
-    margin: 12px;
+  display: inline-block;
+  width: 120px;
+  text-align: center;
+  margin: 12px;
 }
 .prices {
-    margin-top: 6px;
-    line-height: 2em;
+  margin-top: 6px;
+  line-height: 2em;
 }
 .prices > img {
-    display: inline-block;
+  display: inline-block;
 }
 input {
-    width: 3.5em;
+  width: 3.5em;
 }
 </style>
