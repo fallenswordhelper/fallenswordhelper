6a073e7b56670c7400873039d549f69bc48660c1
ahanden <adam.handen@gmail.com>
2023-01-04 11:46:58 -0600

    removed unused async

--- src/modules/chrome/pageSwitcher/loader/injectQuestbook.js
+++ src/modules/chrome/pageSwitcher/loader/injectQuestbook.js
@@ -1,6 +1,6 @@
 import QuestBook from '../../../questbook/QuestBook.svelte';
 import { pcc } from '../../../support/layout';
 
-export default async function injectQuestbook() {
+export default function injectQuestbook() {
   return new QuestBook({ target: pcc() });
 }
