bd612727fd8d30a112158f2ebfdaeb1cdd7dd7ed
atompkins <atompkins@users.noreply.github.com>
2023-01-29 20:31:15 +0000

    codebeat

--- src/modules/common/awaitWidget.js
+++ src/modules/common/awaitWidget.js
@@ -2,19 +2,19 @@ import toLowerCase from './toLowerCase';
 
 const thisWidget = (el, widget, prefix) => $(el).data(`${prefix}${widget}`);
 
-function widgetPromise(el, widget, prefix) {
-  return (resolve) => {
+function waitForWidget(el, widget, prefix, resolve) {
+  $(el).on(`${toLowerCase(widget)}create`, () => {
+    resolve(thisWidget(el, widget, prefix));
+  });
+}
+
+export default function awaitWidget(el, widget, prefix) {
+  return new Promise((resolve) => {
     const preWidget = thisWidget(el, widget, prefix);
     if (preWidget) {
       resolve(preWidget);
     } else {
-      $(el).on(`${toLowerCase(widget)}create`, () => {
-        resolve(thisWidget(el, widget, prefix));
-      });
+      waitForWidget(el, widget, prefix, resolve);
     }
-  };
-}
-
-export default function awaitWidget(el, widget, prefix) {
-  return new Promise(widgetPromise(el, widget, prefix));
+  });
 }
