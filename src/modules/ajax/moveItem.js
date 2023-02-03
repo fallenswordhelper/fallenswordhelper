85ce69a8826bea0e860bfb00ae99c0c514fad86a
ahanden <adam.handen@gmail.com>
2023-02-02 19:56:47 -0600

    ajax

--- src/modules/ajax/moveItem.js
+++ src/modules/ajax/moveItem.js
@@ -2,12 +2,14 @@ import jsonStringify from '../common/jsonStringify';
 import dialog from './dialog';
 import indexAjaxJson from './indexAjaxJson';
 
-export default function moveItem(invIdList, folderId) {
-  return indexAjaxJson({
+export default async function moveItem(invIdList, folderId) {
+  const response = await indexAjaxJson({
     cmd: 'profile',
     subcmd: 'sendtofolder',
     inv_list: jsonStringify(invIdList),
     folder_id: folderId,
     ajax: 1,
-  }).then(dialog);
+  });
+  dialog(response);
+  return response;
 }
--- src/modules/ajax/takeItem.js
+++ src/modules/ajax/takeItem.js
@@ -1,12 +1,14 @@
 import dialog from './dialog';
 import indexAjaxJson from './indexAjaxJson';
 
-export default function takeItem(invId) {
-  return indexAjaxJson({
+export default async function takeItem(invId) {
+  const response = await indexAjaxJson({
     cmd: 'guild',
     subcmd: 'inventory',
     subcmd2: 'takeitem',
     guildstore_id: invId,
     ajax: 1,
-  }).then(dialog);
+  });
+  dialog(response);
+  return response;
 }
