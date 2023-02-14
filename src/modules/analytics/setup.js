2d4edc8b61bf4c6b204fad2660ec99f8c82c4948
Adrian Tompkins <atompkins@users.noreply.github.com>
2023-02-14 13:17:17 +0000

    ga4-is-shit (#513)
    * ga4-is-shit
    
    * deepsource

--- src/modules/analytics/setup.js
+++ src/modules/analytics/setup.js
@@ -70,7 +70,7 @@ function analyticsSetup() {
     window.ga = window.ga || function gafn() {
       ga.q = ga.q || [];
       // eslint-disable-next-line prefer-rest-params
-      ga.q.push(arguments);
+      ga.q.push(arguments); // skipcq: JS-0244
     };
     ga.l = Number(new Date());
   }
@@ -78,23 +78,15 @@ function analyticsSetup() {
   initSite();
 }
 
-function gtagPlayerId() {
-  const pid = playerId();
-  if (pid) gtag('config', 'G-14Y99WX8XL', { user_id: pid });
-}
-
 function gtagSetup() {
   loadScript('https://www.googletagmanager.com/gtag/js?id=G-14Y99WX8XL');
   window.dataLayer = window.dataLayer || [];
-  // eslint-disable-next-line prefer-rest-params
-  window.gtag = window.gtag || function gtag() { window.dataLayer.push(arguments); };
+  window.gtag = window.gtag || function gtag() {
+    // eslint-disable-next-line prefer-rest-params
+    window.dataLayer.push(arguments); // skipcq: JS-0244
+  };
   gtag('js', new Date());
-  gtag('config', 'G-14Y99WX8XL', {
-    app_name: 'fshApp',
-    app_version: `${calf.fshVer}(${calf.calfVer})`,
-    page_location: getPage(),
-  });
-  gtagPlayerId();
+  gtag('config', 'G-14Y99WX8XL');
 }
 
 export default function setup() {
