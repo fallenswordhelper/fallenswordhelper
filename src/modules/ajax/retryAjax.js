e8e2c84a908b82d3312345ce8dae9d949fcc4537
atompkins <atompkins@users.noreply.github.com>
2023-02-18 13:23:29 +0000

    throw ajax error instead of returning null

--- src/modules/ajax/retryAjax.js
+++ src/modules/ajax/retryAjax.js
@@ -1,4 +1,4 @@
-import sendException from '../analytics/sendException';
+// import sendException from '../analytics/sendException';
 import on from '../common/on';
 import AjaxError from './AjaxError';
 
@@ -13,25 +13,25 @@ function beforeSend(xhr) {
   on(window, 'beforeunload', () => xhr.abort());
 }
 
-const ignoreStatus = [0, 503, 504];
-const ignoreTextStatus = ['abort'];
-const ignoreResponse = [
-  'We have encountered an issue with a server connection',
-  'We\'re performing maintenance on the game',
-  'the team have been notified and will get it fixed soon',
-  'uUDRezBqFM4',
-];
+// const ignoreStatus = [0, 503, 504];
+// const ignoreTextStatus = ['abort'];
+// const ignoreResponse = [
+//   'We have encountered an issue with a server connection',
+//   'We\'re performing maintenance on the game',
+//   'the team have been notified and will get it fixed soon',
+//   'uUDRezBqFM4',
+// ];
 
-function ignore(ajaxErr) {
-  return ignoreStatus.includes(ajaxErr.jqXhr.status)
-    || ignoreTextStatus.includes(ajaxErr.jqTextStatus)
-    || ignoreResponse.some((substring) => ajaxErr.jqXhr.responseText.includes(substring));
-}
+// function ignore(ajaxErr) {
+//   return ignoreStatus.includes(ajaxErr.jqXhr.status)
+//     || ignoreTextStatus.includes(ajaxErr.jqTextStatus)
+//     || ignoreResponse.some((substring) => ajaxErr.jqXhr.responseText.includes(substring));
+// }
 
-function handleFailure(options, jqXhr) {
-  const ajaxErr = new AjaxError([options, jqXhr, jqXhr.textStatus, jqXhr.errorThrown]);
-  if (!ignore(ajaxErr)) sendException(ajaxErr.toString(), false);
-}
+// function handleFailure(options, jqXhr) {
+//   const ajaxErr = new AjaxError([options, jqXhr, jqXhr.textStatus, jqXhr.errorThrown]);
+//   if (!ignore(ajaxErr)) sendException(ajaxErr.toString(), false);
+// }
 
 const interval = 900;
 let lastRefill = 0;
@@ -71,7 +71,8 @@ export default async function retryAjax(options, retries = 10) {
     result = await getAjax(options);
   } catch (jqXhr) {
     if (retries && jqXhr.status === 503) return retryAjax(options, retries - 1);
-    handleFailure(options, jqXhr);
+    // handleFailure(options, jqXhr);
+    throw new AjaxError([options, jqXhr, jqXhr.textStatus, jqXhr.errorThrown]);
   }
   return result;
 }
