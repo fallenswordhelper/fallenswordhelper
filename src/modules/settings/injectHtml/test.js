4e0b24d5fa5ac5f0c07a0a1e3a6f21987072a5ad
atompkins <atompkins@users.noreply.github.com>
2023-04-23 15:53:00 +0100

    oops

--- src/modules/settings/injectHtml/test.js
+++
@@ -1,6 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-import { Liquid } from 'liquidjs';
-
-const engine = new Liquid();
-const html = await engine.renderFile('settings.liquid');
-await writeFile('test.html', html);
