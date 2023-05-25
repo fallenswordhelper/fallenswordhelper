e137fd270e79b70ce61f67d8cab425671acbe3ea
atompkins <atompkins@users.noreply.github.com>
2023-05-25 21:15:17 +0100

    reduce-complexity

--- src/modules/common/playerName.js
+++ src/modules/common/playerName.js
@@ -3,14 +3,18 @@ import getText from './getText';
 
 let thisPlayerName = null;
 
+function getStatBarCharacter() {
+  const statBarCharacter = getElementById('statbar-character');
+  return statBarCharacter && getText(statBarCharacter);
+}
+
+const getWindowSelf = () => typeof window.self === 'string' && window.self;
+const getCharacter = () => getStatBarCharacter() || getWindowSelf();
+
 export default function playerName() {
   if (!thisPlayerName) {
-    const statBarCharacter = getElementById('statbar-character');
-    const statBarChar = statBarCharacter && getText(statBarCharacter);
-    const winSelfChar = typeof window.self === 'string' && window.self;
-    if (statBarChar || winSelfChar) {
-      thisPlayerName = statBarChar || winSelfChar;
-    }
+    const selfChar = getCharacter();
+    if (selfChar) thisPlayerName = selfChar;
   }
   return thisPlayerName;
 }
