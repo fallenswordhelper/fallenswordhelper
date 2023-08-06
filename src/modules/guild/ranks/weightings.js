52dbd3bc00e42cc3f044eef7250a075c61e0f0ae
atompkins <atompkins@users.noreply.github.com>
2023-08-06 18:36:10 +0100

    oops

--- src/modules/guild/ranks/weightings.js
+++ src/modules/guild/ranks/weightings.js
@@ -41,7 +41,6 @@ function parseRankData(memberRanks, row) {
   const rankName = getText(rankCell.firstChild); // Text Node
   const thisRank = memberRanks.find((r) => r && r.name === rankName);
   if (thisRank) {
-    console.log('thisRank', thisRank);
     insertHtmlAfterBegin(rankCell, `<span class="fshBlue">(${
       calcPermWeight(thisRank.permissions)
     }) Tax:(${thisRank.tax ?? 0}%)</span> `);
