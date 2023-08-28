81fdac259653c207a82d1e6fb6c517b2a7c46786
atompkins <atompkins@users.noreply.github.com>
2023-08-28 11:48:05 +0100

    dev-flag

--- src/modules/quickbuff/doLabels.js
+++ src/modules/quickbuff/doLabels.js
@@ -1,6 +1,7 @@
 import insertHtmlBeforeEnd from '../common/insertHtmlBeforeEnd';
 import querySelectorArray from '../common/querySelectorArray';
 import setTipped from '../common/setTipped';
+import calf from '../support/calf';
 import { excludeBuff } from './assets';
 import parseBuffLevel from './parseBuffLevel';
 
@@ -8,7 +9,10 @@ function addStamCost(el, nameSpan) {
   const dataTipped = nameSpan.dataset.tipped;
   const { cost } = el.previousElementSibling.dataset;
   setTipped(dataTipped.replace('</center>', `<br>Stamina Cost: ${cost}$&`), nameSpan);
-  insertHtmlBeforeEnd(el.parentNode, `<span></span> <span class="fshDodgerBlue tooltip-bottom-right" data-tooltip="Stamina Cost">${cost}</span>`);
+  if (calf.userIsDev) {
+    insertHtmlBeforeEnd(el.parentNode, '<span></span> <span class="fshDodgerBlue '
+     + `tooltip-bottom-right" data-tooltip="Stamina Cost">${cost}</span>`);
+  }
 }
 
 function canBeDimmed(el, nameSpan) {
