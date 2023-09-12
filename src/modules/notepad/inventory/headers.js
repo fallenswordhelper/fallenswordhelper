d63e023824795839cd76d0a777dcf7116fcb9bb7
Adrian Tompkins <atompkins@users.noreply.github.com>
2023-09-12 18:07:37 +0100

    guards (#577)

--- src/modules/notepad/inventory/headers.js
+++ src/modules/notepad/inventory/headers.js
@@ -1,9 +1,11 @@
+import isArray from '../../common/isArray';
 import setInnerHtml from '../../dom/setInnerHtml';
 import { pcc } from '../../support/layout';
 import { invManFilter } from './assets';
 import { getTheInv } from './buildInv';
 
 export default function headers() {
+  if (!isArray(getTheInv().items)) return;
   let reportTitle = '';
   if (getTheInv().player_id) {
     reportTitle = `<b>&nbsp;Inventory Manager</b> ${
