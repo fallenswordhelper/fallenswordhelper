5ea214e7f7d64a3cd7f5965951c8947695eb48e0
atompkins <atompkins@users.noreply.github.com>
2023-09-17 21:18:39 +0100

    codeclimate 1

--- src/modules/_dataAccess/export/basicItem.js
+++ src/modules/_dataAccess/export/basicItem.js
@@ -6,10 +6,10 @@ const top = (o) => ({
   ...(o.cd && { durability: o.cd }),
   equipped: o.equipped ?? false,
   ...(o.folder_id && { folder_id: o.folder_id }),
-  forge: o.hf ?? 0,
 });
 
 const bottom = (o) => ({
+  forge: o.hf ?? 0,
   guild_tag: o.tg ?? -1,
   inv_id: o.a,
   item_id: o.b,
