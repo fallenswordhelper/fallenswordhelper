8d1da9a5e4bac6a18a5dcbbaa3bce598707e510b
atompkins <atompkins@users.noreply.github.com>
2023-11-06 16:09:29 +0000

    oops

--- src/modules/analytics/sendException.js
+++ src/modules/analytics/sendException.js
@@ -1,13 +1,7 @@
 import stdout from '../support/stdout';
 import getValue from '../system/getValue';
-// import noGa from './noGa';
 
 export default function sendException(desc, fatal) {
   if (getValue('betaOptIn')) stdout('sendException', desc); //  sendException
-  // if (noGa()) return;
-  // ga('fshApp.send', 'exception', {
-  //   exDescription: desc,
-  //   exFatal: fatal,
-  // });
   gtag('event', 'exception', { description: desc, fatal });
 }
