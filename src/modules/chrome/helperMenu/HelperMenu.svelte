0f84c73b82509e80e93122716192069a5935b543
ahanden <adam.handen@gmail.com>
2023-12-30 22:19:17 -0600

    Linting fixes

--- src/modules/chrome/helperMenu/HelperMenu.svelte
+++ src/modules/chrome/helperMenu/HelperMenu.svelte
@@ -1,64 +1,79 @@
 <script>
   import { onMount } from 'svelte';
   import draggable from '../../common/draggable';
-  import playerLink from '../../common/playerLink';
   import querySelector from '../../common/querySelector';
+  import { playerIdUrl } from '../../support/constants';
   import getValue from '../../system/getValue';
   import functionLookup from './functionLookup';
 
   const isFixed = getValue('keepHelperMenuOnScreen');
   const isDraggable = getValue('draggableHelperMenu');
-  let showMenu = false;
 
   onMount(() => {
     if (isDraggable) draggable(querySelector('#helperMenu'));
   });
+
+  let showMenu = false;
+  function toggle() {
+    showMenu = !showMenu;
+  }
 </script>
 <div id="helperMenu"
   class="helperMenu"
   class:helperMenuFixed={ isFixed }
-  class:helperMenuMove={ isDraggable }
-  on:click={ () => showMenu = !showMenu }>
-  Helper Menu
+  class:helperMenuMove={ isDraggable }>
+  <button type="button" id="toggle" on:click={ toggle }>Helper Menu</button>
   <div id="helperMenuDiv" class="helperMenuDiv" class:helperMenuShow={ showMenu }>
     { #each functionLookup as menuSection }
       <h2>{ menuSection.section }</h2>
       <ul>{ #each menuSection.menu as menuItem }
         <li>
-      { #if menuItem.type === 'button' }
-      <button type="button" on:click={ () => menuItem.caller(menuItem.label, menuItem.f) }>
-        { menuItem.label }
-      </button>
-      { :else if menuItem.type === 'link' }
-        <a href={ menuItem.href }>{ menuItem.label }</a>
-      { /if }
-      { #if Object.prototype.hasOwnProperty.call(menuItem, 'beta') && menuItem.beta }<sup class="fshRed">beta</sup>{ /if }
+          { #if menuItem.type === 'button' }
+            <button type="button" on:click={ () => menuItem.caller(menuItem.label, menuItem.f) }>
+              { menuItem.label }
+            </button>
+          { :else if menuItem.type === 'link' }
+            <a href={ menuItem.href }>{ menuItem.label }</a>
+          { /if }
+          { #if Object.prototype.hasOwnProperty.call(menuItem, 'beta') && menuItem.beta }<sup class="fshRed">beta</sup>{ /if }
+        </li>
       { /each }
       </ul>
     { /each }
     <h2>FSH developer quick links</h2>
     <ul>
-      <li><button type="button" class="helperDevBtn" onclick="openQuickMsgDialog('Lusterless');">PM</button> { @html playerLink(1963510, 'PointyHair') }</li>
-      <li><button type="button" class="helperDevBtn" onclick="openQuickMsgDialog('Lusterless');">PM</button> { @html playerLink(1674838, 'Lusterless') }</li>
+      <li>
+        <button type="button" class="helperDevBtn" onclick="openQuickMsgDialog('Lusterless');">
+          PM
+        </button>
+        <a href="{ playerIdUrl }1963510">PointyHair</a></li>
+      <li>
+        <button type="button" class="helperDevBtn" onclick="openQuickMsgDialog('Lusterless');">
+          PM
+        </button>
+        <a href="{ playerIdUrl }1674838">Lusterless</a></li>
     </ul>
   </div>
 </div>
 <style>
-
 .helperMenu {
-  color: yellow;
   cursor: pointer;
-  font-size: 13px;
-  font-weight: bold;
   left: 0;
   position: absolute;
-  text-align: center;
-  text-decoration: underline;
   top: 0;
-  white-space: nowrap;
+  text-align: center;
   z-index: 75;
+  white-space: nowrap;
 }
-
+#toggle {
+  color: yellow;
+  font-size: 13px;
+  font-weight: bold;
+  text-decoration: underline;
+  text-align: center;
+  background: none;
+  border: none;
+}
 .helperMenuDiv {
   background-color: #e6c270;
   border:3px solid #ccbb77;
@@ -78,9 +93,8 @@
   transition: opacity 100ms linear, visibility 0s linear;
   visibility: visible;
 }
-.helperMenuFixed {position: fixed;}
-.helperMenuMove {cursor: move;}
-
+.helperMenuFixed { position: fixed; }
+.helperMenuMove #toggle { cursor: move; }
 h2 {
   background: #e0e0e0;
   font: bold 13px Arial, Helvetica, sans-serif;
