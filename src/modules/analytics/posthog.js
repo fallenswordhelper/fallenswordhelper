66221ac81653182c971e3588b26936e5b62344a2
Adrian Tompkins <atompkins@users.noreply.github.com>
2024-09-09 15:30:01 +0100

    avoid-posthog-failure (#670)

--- src/modules/analytics/posthog.js
+++ src/modules/analytics/posthog.js
@@ -2,12 +2,16 @@ import ph from 'posthog-js';
 
 export default function posthog() {
   if (defineUserIsDev) return;
-  ph.init(
-    'phc_rlYc31x7Pvbc1XzLuePdrHh1JIFq4ClkniLjoqs0V1N',
-    {
-      api_host: 'https://us.i.posthog.com',
-      person_profiles: 'identified_only',
-      persistence: 'localStorage',
-    },
-  );
+  try {
+    ph.init(
+      'phc_rlYc31x7Pvbc1XzLuePdrHh1JIFq4ClkniLjoqs0V1N',
+      {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only',
+        persistence: 'localStorage',
+      },
+    );
+  } catch (error) {
+    // ignore bad init
+  }
 }
