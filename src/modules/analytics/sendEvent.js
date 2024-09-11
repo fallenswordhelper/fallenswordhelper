14e47dc4e19b26a78150f81809936b2205591e38
atompkins <atompkins@users.noreply.github.com>
2024-09-11 13:10:32 +0100

    reconfigure-posthog

--- src/modules/analytics/sendEvent.js
+++ src/modules/analytics/sendEvent.js
@@ -1,8 +1,25 @@
 import stdout from '../support/stdout';
-import noGa from './noGa';
+import { phEvent } from './posthog';
+
+function spaceToUnderscore(str) {
+  return str.replaceAll(' ', '_');
+}
+
+function buildEvent(eventCategory, eventAction) {
+  let phe = spaceToUnderscore(eventCategory);
+  if (eventAction) phe += `__${spaceToUnderscore(eventAction)}`;
+  return phe;
+}
+
+function buildProps(eventLabel) {
+  if (eventLabel) return { eventLabel };
+  return null;
+}
 
 export default function sendEvent(eventCategory, eventAction, eventLabel) {
-  if (noGa()) { return; }
-  ga('fshApp.send', 'event', eventCategory, eventAction, eventLabel);
-  if (defineUserIsDev) stdout('sendEvent', eventCategory, eventAction, eventLabel);
+  if (!eventCategory) {
+    if (defineUserIsDev) stdout('sendEvent', eventCategory, eventAction, eventLabel);
+    return;
+  }
+  phEvent(buildEvent(eventCategory, eventAction), buildProps(eventLabel));
 }
