7c69fbd142b8e5f8057d75130e46190322cea7c8
Adrian Tompkins <atompkins@users.noreply.github.com>
2024-10-03 11:54:45 +0100

    eslint-prettier (#690)

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.greasemonkey,
+        ...globals.jquery,
+        ...globals.node,
+        DataTable: 'readonly',
+        defineCalfPath: 'readonly',
+        defineCalfVer: 'readonly',
+        defineDataTablesPath: 'readonly',
+        defineEnvironment: 'readonly',
+        defineUserIsDev: 'readonly',
+        defineVersion: 'readonly',
+        GameController: 'readonly',
+        GameData: 'readonly',
+        gtag: 'readonly',
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  ...eslintPluginSvelte.configs['flat/prettier'],
+  { ignores: ['dist/'] },
+  { rules: { 'no-unused-vars': ['error', { caughtErrors: 'none' }] } },
+];
