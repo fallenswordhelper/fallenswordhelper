202101c8b101b9df4a9de1b8c3b9e79a9d56193c
atompkins <atompkins@users.noreply.github.com>
2024-12-19 09:37:09 +0000

    oops

--- src/modules/chrome/lookForHcsData/priorityThree/seTracker.js
+++ src/modules/chrome/lookForHcsData/priorityThree/seTracker.js
@@ -2,7 +2,7 @@ import jQueryPresent from '../../../common/jQueryPresent';
 import trackerPrefStore from '../../../notepad/superelite/trackerPrefStore';
 import calf from '../../../support/calf';
 
-export default async function seTracker() {
+export default function seTracker() {
   if (jQueryPresent() && calf.enableSeTracker) {
     trackerPrefStore.set(calf.enableSeTracker);
   }
