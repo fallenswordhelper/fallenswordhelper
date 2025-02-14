776737483388602de00ba438d815038bfa5c1ea4
atompkins <atompkins@users.noreply.github.com>
2025-02-14 06:39:30 +0000

    trade-add-send-btn

--- src/modules/trade/Trade.svelte
+++ src/modules/trade/Trade.svelte
@@ -1,7 +1,20 @@
 <script>
   import sendEvent from '../analytics/sendEvent';
+  import clickThis from '../common/clickThis';
+  import getElementById from '../common/getElementById';
   import QuickSelectClasses from '../common/QuickSelectClasses.svelte';
+
+  function sendBtn() {
+    sendEvent('Trade', 'doSend');
+    clickThis(getElementById('send-items'));
+  }
 </script>
+
+{#if getElementById('send-items')}
+  <div>
+    <button class="custombutton" on:click={sendBtn} type="button">Send</button>
+  </div>
+{/if}
 
 <QuickSelectClasses
   on:perf={() => sendEvent('Trade', 'doPerf')}
@@ -9,3 +22,15 @@
   on:toggle={() => sendEvent('Trade', 'toggleSelectST')}
   wantsTagged="1"
 />
+
+<style>
+  div {
+    margin: 10px auto 10px auto;
+    text-align: center;
+  }
+  button {
+    font-size: 12px;
+    font-family: Helvetica, Arial, sans-serif;
+    width: 100px;
+  }
+</style>
