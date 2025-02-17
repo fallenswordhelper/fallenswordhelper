f1fb0ff4fb09593bbbb8d0765c851b804d115676
atompkins <atompkins@users.noreply.github.com>
2025-02-17 10:12:43 +0000

    deprecated-links

--- src/modules/guild/inventory/guildReport/prepareChildRows.js
+++ src/modules/guild/inventory/guildReport/prepareChildRows.js
@@ -1,13 +1,5 @@
-import sendEvent from '../../../analytics/sendEvent';
-import potReport from '../../../chrome/pageSwitcher/loader/potReport';
 import batch from '../../../common/batch';
-import createButton from '../../../common/cElement/createButton';
-import createSpan from '../../../common/cElement/createSpan';
-import closestTable from '../../../common/closestTable';
 import insertElement from '../../../common/insertElement';
-import insertElementAfterBegin from '../../../common/insertElementAfterBegin';
-import onclick from '../../../common/onclick';
-import querySelector from '../../../common/querySelector';
 import querySelectorAll from '../../../common/querySelectorAll';
 import { pcc } from '../../../support/layout';
 import makeFastRecall from './makeFastRecall';
@@ -24,43 +16,12 @@ function doSpan(el) {
   nodeArray.push(makeFastRecall(el));
 }
 
-function makeBtn() {
-  const btn = createButton({
-    className: 'fshBl pot-report-button',
-    textContent: 'Pot Report',
-  });
-  onclick(btn, () => {
-    sendEvent('GuildReport', 'Pot Report Moved');
-    potReport();
-  });
-  return btn;
-}
-
-function potReportNote() {
-  const taggedItemReport = closestTable(
-    querySelector('#pCC table table').parentNode,
-  );
-  const top = taggedItemReport.children?.[0]?.rows?.[0]?.cells?.[0];
-  if (top) {
-    top.classList.add('fshRelative');
-    const btn = makeBtn();
-    const spn = createSpan({
-      className: 'pot-report-moved',
-      textContent: ' moved to Helper Menu',
-    });
-    insertElementAfterBegin(spn, btn);
-    insertElement(top, spn);
-  }
-}
-
 function finishSpan() {
-  batch([3, nodeArray, 0, doPaintChild, potReportNote]);
+  batch([3, nodeArray, 0, doPaintChild]);
 }
 
 export default function prepareChildRows() {
-  if (!pcc()) {
-    return;
-  }
+  if (!pcc()) return;
   nodeList = querySelectorAll(
     '#pCC table table tr:not(.fshHide) td:nth-of-type(3n)',
   );
