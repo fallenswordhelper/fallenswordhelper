8e17c559898f2064ce1e71378c31b024d400c4fe
atompkins <atompkins@users.noreply.github.com>
2025-03-04 20:21:35 +0000

    components-feedback

--- src/modules/profile/components/Count.svelte
+++ src/modules/profile/components/Count.svelte
@@ -1,15 +1,20 @@
 <script>
+  import { SvelteMap } from 'svelte/reactivity';
   import daComponents from '../../_dataAccess/daComponents';
-  import objectToMap from '../../common/objectToMap';
   import uniq from '../../common/uniq';
   import { compStore, total } from './componentsStore';
   import CountRow from './CountRow.svelte';
   import getAsyncData from './getAsyncData';
+  import entries from '../../common/entries';
 
   const { dispatchDelType } = $props();
 
+  function objectToMap(obj) {
+    return new SvelteMap(entries(obj));
+  }
+
   function rollupComponents(componentsJson) {
-    const aggregate = new Map(
+    const aggregate = new SvelteMap(
       uniq(componentsJson, 'b').map((o) => [
         o.b,
         objectToMap({
--- src/modules/profile/components/componentsStore.js
+++ src/modules/profile/components/componentsStore.js
@@ -1,6 +1,7 @@
+import { SvelteMap } from 'svelte/reactivity';
 import { derived, writable } from 'svelte/store';
 
-export const compStore = writable(new Map());
+export const compStore = writable(new SvelteMap());
 
 export const total = derived(compStore, ($compStore) =>
   [...$compStore].reduce((acc, [, m]) => acc + (m.get('del')?.length ?? 0), 0),
