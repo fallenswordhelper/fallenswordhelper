05d96d074cf16ac83789958b15ea02edbac965c3
atompkins <atompkins@users.noreply.github.com>
2025-03-10 07:31:32 +0000

    blank

---
+++ src/modules/notepad/blank.js
@@ -0,0 +1,34 @@
+import dynamicAlert from '../alert/dynamicAlert';
+import createButton from '../common/cElement/createButton';
+import insertElement from '../common/insertElement';
+import onclick from '../common/onclick';
+import { pcc } from '../support/layout';
+import mountGuildLog from './guildLog/mountGuildLog.svelte';
+import mountPotReport from './potReport/mountPotReport.svelte';
+import mountQuickExtract from './quickExtract/mountQuickExtract.svelte';
+import mountWhosGotWhat from './whosGotWhat/mountWhosGotWhat.svelte';
+
+let index = 0;
+
+const msgs = ['first', 'second', 'third'];
+
+function alertHdl() {
+  dynamicAlert(msgs[index]);
+  index += 1;
+  if (index === 3) index = 0;
+}
+
+function addBtn(textContent, btnHdl) {
+  const btn = createButton({ textContent });
+  insertElement(pcc(), btn);
+  onclick(btn, btnHdl);
+}
+
+export default function blank() {
+  if (!defineUserIsDev || location.search !== '?cmd=notepad&blank=1') return;
+  addBtn('Alert', alertHdl);
+  addBtn('WhosGotWhat', mountWhosGotWhat);
+  addBtn('Quick Extract', mountQuickExtract);
+  addBtn('Guild Log', mountGuildLog);
+  addBtn('Pot Report', mountPotReport);
+}
