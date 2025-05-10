9e17d89c8dc3d7513faa4a320342fa764d038148
ahanden <adam.handen@gmail.com>
2025-05-10 16:49:19 -0500

    changed bazaar purchase to generic fallback

--- src/modules/_dataAccess/daBazaarBuy.js
+++ src/modules/_dataAccess/daBazaarBuy.js
@@ -1,21 +1,14 @@
-import indexAjaxDoc from '../ajax/indexAjaxDoc';
 import buyitem from '../app/potionbazaar/buyitem';
-import infoBox from '../common/infoBox';
 import $dataAccess from './$dataAccess';
+import aGenericFallback from './fallbacks/aGenericFallback';
 
-async function bazaarBuy(item, quantity) {
-  const doc = await indexAjaxDoc({
+function bazaarBuy(item, quantity) {
+  return aGenericFallback({
     cmd: 'potionbazaar',
     subcmd: 'buyitem',
     item_id: item,
     quantity,
-  });
-  if (!doc) return { s: false };
-  const info = infoBox(doc);
-  if (info === 'You purchased the item!') {
-    return { r: Array(quantity), s: true };
-  }
-  return { e: { message: info }, s: false };
+  },'You purchased the item!');
 }
 
 export default function daBazaarBuy(item, quantity) {
