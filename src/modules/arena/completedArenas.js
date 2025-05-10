88dfc0aacb5a50cff502cdb1aa843d36017e3fcd
ahanden <adam.handen@gmail.com>
2025-05-10 15:16:23 -0500

    Reduced fn length and reordered code for checks

--- src/modules/arena/completedArenas.js
+++ src/modules/arena/completedArenas.js
@@ -14,6 +14,27 @@ const lastPage = getText(
 
 const inputTr = querySelector('#pCC #page').closest('tr');
 
+const getTarget = (context=document) =>
+  querySelector('#pCC input[value=Go]', context).closest('tr');
+
+
+function createSpinner() {
+  const spinner = createDiv();
+  spinner.style.cssText = `
+    animation: fshSpinner 0.6s linear infinite;
+    border: 2px solid #ccc;
+    border-radius: 50%;
+    border-top-color: #07d;
+    height: 8px;
+    width: 8px;
+    margin: 0px 4px;
+    display: none;
+  `;
+  return spinner;
+}
+
+const spinner = createSpinner();
+
 async function gotoPage(page) {
   if (page === pageId) {
     return;
@@ -50,54 +71,26 @@ function gotoNextPage() {
   gotoPage(Math.min(pageId + 1, lastPage));
 }
 
-const getTarget = (context=document) =>
-  querySelector('#pCC input[value=Go]', context).closest('tr');
-
-const createButton = (props) => createInput({ type: 'button', ...props });
+function createButton(value, onclick) {
+  return createInput({
+    value,
+    onclick,
+    type: 'button',
+  });
+}
 
 function createButtonsTd() {
   const inputTd = cElement('td');
   inputTd.append(
-    createButton({
-      value: 'Go',
-      onclick: () => gotoPage(querySelector('#page').value),
-    }), ' ',
-    createButton({
-      value: '<<',
-      onclick: gotoFirstPage,
-    }), ' ',
-    createButton({
-      value: '<',
-      onclick: gotoPrevPage,
-    }), ' ',
-    createButton({
-      value: '>',
-      onclick: gotoNextPage,
-    }), ' ',
-    createButton({
-      value: '>>',
-      onclick: gotoLastPage,
-    }),
+    createButton('Go', () => gotoPage(querySelector('#page').value)), ' ',
+    createButton('<<', gotoFirstPage), ' ',
+    createButton('<', gotoPrevPage), ' ',
+    createButton('>', gotoNextPage), ' ',
+    createButton('>>', gotoLastPage),
   );
   return inputTd;
 }
 
-function createSpinner() {
-  const spinner = createDiv();
-  spinner.style.cssText = `
-    animation: fshSpinner 0.6s linear infinite;
-    border: 2px solid #ccc;
-    border-radius: 50%;
-    border-top-color: #07d;
-    height: 8px;
-    width: 8px;
-    margin: 0px 4px;
-    display: none;
-  `;
-  return spinner;
-}
-
-const spinner = createSpinner();
 
 export default function completedArenas() {
   getTarget().children[1].replaceWith(createButtonsTd());
