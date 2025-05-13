765e7777c9b56a648b2b77b2dfa19cec848e655c
ahanden <adam.handen@gmail.com>
2025-05-13 13:54:23 -0500

    Added spinner during ajax calls

--- src/modules/buffmarket/injectManage.js
+++ src/modules/buffmarket/injectManage.js
@@ -1,3 +1,5 @@
+import './manage.css';
+import cElement from '../common/cElement/cElement';
 import createDiv from '../common/cElement/createDiv';
 import onclick from '../common/onclick';
 import querySelector from '../common/querySelector';
@@ -8,9 +10,12 @@ import daFeatureBuffPackage from '../_dataAccess/daFeatureBuffPackage';
 import daDeleteBuffPackage from '../_dataAccess/daDeleteBuffPackage';
 import { pcc } from '../support/layout';
 import insertElementAfterBegin from '../common/insertElementAfterBegin';
+import closestTr from '../common/closestTr';
+import closestTd from '../common/closestTd';
 
 const getPackageId = (button) =>
   button.getAttribute('onclick').match(/id=(\d+)/)[1];
+
 
 function createInfoBox() {
   const wrapper = createDiv();
@@ -54,11 +59,27 @@ function replaceOnClick(target, fn) {
   onclick(target, fn)
 }
 
-async function toggleBuffPackage(event) {
+function makeSpinnerCell() {
+  const wrapper = cElement('td');
+  const spinner = createDiv({ className: 'fshBuffmarketSpinner' });
+  wrapper.append(spinner);
+  return wrapper;
+}
+
+async function doDaAction(event, daFunction) {
   const packageId = event.target.getAttribute('data-packageId');
-  const response = await daToggleBuffPackage(packageId);
+  const spinner = makeSpinnerCell();
+  const originalInputs = closestTd(event.target);
+  originalInputs.replaceWith(spinner);
+  const response = await daFunction(packageId);
+  spinner.replaceWith(originalInputs);
+  return response;
+}
+
+async function toggleBuffPackage(event) {
+  const response = await doDaAction(event, daToggleBuffPackage);
   if (response?.s === true) {
-    const statusTd = event.target.closest('tr').children[3];
+    const statusTd = closestTr(event.target).children[3];
     statusTd.innerText = getText(statusTd) == 'Yes' ? 'No' : 'Yes';
     doInfoBox('Buff Package toggled!');
   } else {
@@ -68,10 +89,9 @@ async function toggleBuffPackage(event) {
 
 async function deleteBuffPackage(event) {
   if (confirm('Are you sure you want to delete this package?')) {
-    const packageId = event.target.getAttribute('data-packageId');
-    const response = await daDeleteBuffPackage(packageId);
+    const response = await doDaAction(event, daDeleteBuffPackage);
     if (response?.s === true) {
-      const tr = event.target.closest('tr');
+      const tr = closestTr(event.target);
       tr.nextElementSibling.remove();
       tr.remove();
       doInfoBox('Buff Package deleted!');
@@ -82,14 +102,13 @@ async function deleteBuffPackage(event) {
 }
 
 async function featureBuffPackage(event) {
-  const packageId = event.target.getAttribute('data-packageId');
-  const response = await daFeatureBuffPackage(packageId);
-    if (response?.s === true) {
-      event.target.closest('tr').children[4].innerText = 'Yes';
-      doInfoBox('Buff Package featured for 24 hours!');
-    } else {
-      doInfoBox(response?.e?.message ?? 'Server Error');
-    }
+  const response = await doDaAction(event, daFeatureBuffPackage);
+  if (response?.s === true) {
+    closestTr(event.target).children[4].innerText = 'Yes';
+    doInfoBox('Buff Package featured for 24 hours!');
+  } else {
+    doInfoBox(response?.e?.message ?? 'Server Error');
+  }
 }
 
 export default function injectManage() {
