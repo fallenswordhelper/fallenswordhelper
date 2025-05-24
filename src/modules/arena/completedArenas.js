79e22e2bfcfcc90add4ec1400749a1238cf1618c
Adam Handen <adam.handen@gmail.com>
2025-05-24 12:49:11 -0500

    Ajaxified completed arenas (#771)
    * Ajaxified completed arenas
    
    * Reduced fn length and reordered code for checks

--- src/modules/arena/completedArenas.js
+++ src/modules/arena/completedArenas.js
@@ -1,18 +1,56 @@
+import cElement from '../common/cElement/cElement';
+import createDiv from '../common/cElement/createDiv';
 import sendEvent from '../analytics/sendEvent';
 import createInput from '../common/cElement/createInput';
 import getText from '../common/getText';
-import insertElementAfter from '../common/insertElementAfter';
-import insertElementBefore from '../common/insertElementBefore';
-import insertHtmlAfterEnd from '../common/insertHtmlAfterEnd';
-import insertHtmlBeforeBegin from '../common/insertHtmlBeforeBegin';
-import interceptSubmit from '../common/interceptSubmit';
-import navigateTo from '../common/navigateTo';
-import onclick from '../common/onclick';
 import querySelector from '../common/querySelector';
-import { arenaUrl } from '../support/constants';
+import indexAjaxDoc from '../ajax/indexAjaxDoc';
 
-function gotoPage(pageId) {
-  navigateTo(`${arenaUrl}completed&page=${pageId}`);
+let pageId = querySelector('#pCC #page').value;
+
+const lastPage = getText(
+    querySelector('#pCC input[value=Go]').parentNode.previousElementSibling,
+  ).replace(/\D/g, '');
+
+const inputTr = querySelector('#pCC #page').closest('tr');
+
+const getTarget = (context=document) =>
+  querySelector('#pCC input[value=Go]', context).closest('tr');
+
+
+function createSpinner() {
+  const spinner = createDiv();
+  spinner.style.cssText = `
+    animation: fshSpinner 0.6s linear infinite;
+    border: 2px solid #ccc;
+    border-radius: 50%;
+    border-top-color: #07d;
+    height: 8px;
+    width: 8px;
+    margin: 0px 4px;
+    display: none;
+  `;
+  return spinner;
+}
+
+const spinner = createSpinner();
+
+async function gotoPage(page) {
+  if (page === pageId) {
+    return;
+  }
+  pageId = page;
+
+  inputTr.querySelector('#page').value = page;
+  spinner.style.display = 'inline-block';
+  const doc = await indexAjaxDoc({
+    cmd: 'arena',
+    subcmd: 'completed',
+    page: pageId,
+  });
+  getTarget(doc).replaceWith(inputTr);
+  querySelector('#pCC').replaceWith(querySelector('#pCC', doc));
+  spinner.style.display = 'none';
 }
 
 function gotoFirstPage() {
@@ -20,42 +58,43 @@ function gotoFirstPage() {
   gotoPage(1);
 }
 
-const getTarget = (inputValue) =>
-  querySelector(`#pCC input[value="${inputValue}"]`);
-const newButton = (newValue) =>
-  createInput({ type: 'button', value: newValue });
-
-function injectStartButton(startButton, prevButton) {
-  insertElementBefore(startButton, prevButton);
-  insertHtmlAfterEnd(startButton, '&nbsp;');
-  onclick(startButton, gotoFirstPage);
-}
-
-const lastPage = () =>
-  getText(
-    querySelector('#pCC input[value="Go"]').parentNode.previousElementSibling,
-  ).replace(/\D/g, '');
-
 function gotoLastPage() {
   sendEvent('arena completed', 'gotoLastPage');
-  gotoPage(lastPage());
+  gotoPage(lastPage);
 }
 
-function injectFinishButton(finishButton, nextButton) {
-  insertElementAfter(finishButton, nextButton);
-  insertHtmlBeforeBegin(finishButton, '&nbsp;');
-  onclick(finishButton, gotoLastPage);
+function gotoPrevPage() {
+  gotoPage(Math.max(1, pageId - 1));
 }
 
-function genericButton(inputValue, newValue, callback) {
-  const targetButton = getTarget(inputValue);
-  if (!targetButton) return;
-  const extraButton = newButton(newValue);
-  callback(extraButton, targetButton);
+function gotoNextPage() {
+  gotoPage(Math.min(pageId + 1, lastPage));
 }
+
+function createButton(value, onclick) {
+  return createInput({
+    value,
+    onclick,
+    type: 'button',
+  });
+}
+
+function createButtonsTd() {
+  const inputTd = cElement('td');
+  inputTd.append(
+    createButton('Go', () => gotoPage(querySelector('#page').value)), ' ',
+    createButton('<<', gotoFirstPage), ' ',
+    createButton('<', gotoPrevPage), ' ',
+    createButton('>', gotoNextPage), ' ',
+    createButton('>>', gotoLastPage),
+  );
+  return inputTd;
+}
+
 
 export default function completedArenas() {
-  genericButton('<', '<<', injectStartButton);
-  genericButton('>', '>>', injectFinishButton);
-  interceptSubmit();
+  getTarget().children[1].replaceWith(createButtonsTd());
+
+  const td = querySelector('#pCC #page').closest('td');
+  td.insertBefore(spinner, td.firstChild);
 }
