cac1df4a5f2b531dc75f1444c9cd98e25ce42309
ahanden <adam.handen@gmail.com>
2025-06-03 12:44:29 -0500

    Fixed compatibility with stats text wrapping

--- src/modules/profile/backpack/widget/updateStatistics.js
+++ src/modules/profile/backpack/widget/updateStatistics.js
@@ -1,7 +1,6 @@
 import profile from '../../../_dataAccess/export/profile';
 import querySelector from '../../../common/querySelector';
 import setText from '../../../dom/setText';
-import createDiv from '../../../common/cElement/createDiv';
 import playerName from '../../../common/playerName';
 import addCommas from '../../../system/addCommas';
 
@@ -17,13 +16,13 @@ function updateBaseStats(response) {
   ['attack', 'defense', 'damage', 'armor', 'hp']
     .forEach((statName) =>{
       const statTd = querySelector(`td#stat-${statName}`);
-      setText(addCommas(response[statName]), statTd);
-      const bonuseKey = `bonus_${statName}`
-      statTd.append(
-        createDiv({
-          className: 'profile-stat-bonus',
-          innerText: `(+${addCommas(response[bonuseKey])})`,
-        }),
+      setText(
+        addCommas(response[statName]),
+        statTd.childNodes[0],
+      );
+      setText(
+        addCommas(response[`bonus_${statName}`]),
+        querySelector('.profile-stat-bonus', statTd),
       );
   });
 }
@@ -33,5 +32,4 @@ export default async function updateStatistics(enhancementsApp) {
   updateBaseStats(response);
   updateStamina(response);
   enhancementsApp.updateEnhancements(response._enhancements);
-  //updateEnhancements(response);
 }
