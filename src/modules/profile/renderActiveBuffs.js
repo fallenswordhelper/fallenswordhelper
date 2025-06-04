435463716dbef12f3e287c6a1e0e7d48f6bfdcb3
ahanden <adam.handen@gmail.com>
2025-06-04 12:18:00 -0500

    Reduced ABC of buffElement

--- src/modules/profile/renderActiveBuffs.js
+++ src/modules/profile/renderActiveBuffs.js
@@ -6,7 +6,6 @@ import createDiv from '../common/cElement/createDiv';
 import cElement from '../common/cElement/cElement';
 import onclick from '../common/onclick';
 import setTipped from '../common/setTipped';
-import setText from '../dom/setText';
 import querySelector from '../common/querySelector';
 import { cdn } from '../system/system';
 
@@ -19,7 +18,7 @@ function formatDuration(secondsLeft) {
   return `${secondsLeft}s`;
 }
 
-function deactivateBuff(buff) {
+function deactivateBuff(div, buff) {
   mount(
     ModalConfirm,
     {
@@ -30,10 +29,26 @@ function deactivateBuff(buff) {
         resolve: async (doDeactivate) => {
           if (!doDeactivate) return;
           await daRemoveSkill(buff.id);
+          div.remove();
         }
       }
     },
   );
+}
+
+function buffImg(buff) {
+  const img = cElement('img', {
+    src: `${cdn}skills/${buff.id}.png`,
+    alt: buff.name,
+    width: 25,
+    height: 24,
+    className: 'tip-static',
+  });
+  setTipped(
+    `<div style='width: 250px; font-size: 12px;'><center><span style='color: #0f0'><b>${buff.name}</b> (Level: ${buff.level})</span><br>${buff.tooltip}<br>[Click to De-Activate]</center></div>`,
+    img,
+  );
+  return img;
 }
 
 function buffElement(buff) {
@@ -45,21 +60,11 @@ function buffElement(buff) {
     font-size: 10px;
     text-wrap: nowrap;
   `;
-  onclick(div, () => deactivateBuff(buff));
-
-  const img = cElement('img', {
-    src: `${cdn}skills/${buff.id}.png`,
-    alt: buff.name,
-    width: 25,
-    height: 24,
-    className: 'tip-static',
-  });
-  setTipped(`<div style='width: 250px; font-size: 12px;'><center><span style='color: #0f0'><b>${buff.name}</b> (Level: ${buff.level})</span><br>${buff.tooltip}<br>[Click to De-Activate]</center></div>`, img);
-  div.append(img);
+  onclick(div, () => deactivateBuff(div, buff));
+  div.append(buffImg(buff));
 
   if (getValue('showBuffLevel')) {
-    const buffLevel = createDiv();
-    setText(`(${buff.level})`, buffLevel);
+    const buffLevel = createDiv({ innerText: `(${buff.level})` });
     buffLevel.style.fontWeight = 'bold';
     div.append(buffLevel);
   }
