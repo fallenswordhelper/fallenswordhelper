632518aa57f5d0e8be50df74045b78f78e696968
ahanden <adam.handen@gmail.com>
2025-08-19 22:27:18 -0500

    Fixed item display names on drop item page and guild store item page

--- src/modules/guild/inventory/storeitems/getItems.js
+++ src/modules/guild/inventory/storeitems/getItems.js
@@ -8,7 +8,6 @@ import getInv from './getInv';
 let itemsprm = null;
 
 function updateName(checkboxes, item) {
-  if (item.item_id !== 13699) return item.item_name;
   const thisItem = checkboxes.find((cb) => cb.value === String(item.inv_id));
   if (!thisItem) return item.item_name;
   return getTextTrim(thisItem.parentNode.parentNode.children[2]);
