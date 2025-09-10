1061bb519b8d1517ec70928a49eb421634a7a7b5
ahanden <adam.handen@gmail.com>
2025-09-09 21:36:17 -0500

    Stats more inline with old eval

--- src/modules/world/newMap/viewCreature/CombatEval.svelte
+++ src/modules/world/newMap/viewCreature/CombatEval.svelte
@@ -1,8 +1,13 @@
 <script>
+  import calf from '../../../support/calf';
   let { title = '', player = {}, enemy = {}, buffs = {}, bonuses = {}} = $props();
 
-  const willIHit = player.attack > enemy.defense;
-  const willIBeHit = enemy.attack > player.defense;
+  const altVal = d => Math.ceil(calf.generalVariable * d);
+
+  const willIHit = $derived(player.attack > altVal(enemy.defense));
+  const willIBeHit = $derived(altVal(enemy.attack) > player.defense);
+  const excessDamage = $derived(player.damage - enemy.hp - altVal(enemy.armor));
+
 
   export function update(a, b, c, d) {
     player = a;
@@ -32,75 +37,65 @@
     return buffNames.join(', ');
   }
 
-  const statLabels = [
-    { label: 'Attack', key: 'attack' },
-    { label: 'Damage', key: 'damage' },
-    { label: 'Defense', key: 'defense' },
-    { label: 'Armor', key: 'armor' },
-    { label: 'HP', key: 'hp' },
-  ];
 </script>
 &nbsp;
 <div id="combat-eval-header">{title}</div>
 <div id="combat-eval-bonuses">
   <table id="combat-eval-player-bonuses">
-    <thead><tr><th colspan="2">Player Stats</th></tr></thead>
     <tbody>
       <tr>
         <td>Will I hit it:</td>
-        <td>{willIHit ? 'Yes' : 'No'}</td>
+        {#if willIHit}
+        <td>Yes</td>
+        <td>({player.attack - altVal(enemy.defense)} excess attack)</td>
+        {:else}
+        <td>No</td>
+        <td>(short {altVal(enemy.defense) - player.attack} attack)</td>
+        {/if}
       </tr>
       <tr>
         <td># Hits to kill it:</td>
-        <td>{willIHit ? player.hits : '-'}</td>
+        {#if !willIHit}
+        <td>-</td>
+        {:else}
+        <td>{player.hits > 20 ? ">20" : player.hits}</td>
+        {#if player.hits > 1}
+        <td>({altVal(enemy.armor) + enemy.hp - player.damage} more damage to one-hit)</td>
+        {:else}
+        <td>({excessDamage} excess damage, {Math.floor(excessDamage / enemy.hp * 100)}% of enemy HP)</td>
+        {/if}
+        {/if}
       </tr>
-      {#each statLabels as {label, key} (key)}
-      <tr>
-        <td>{label}:</td>
-        <td>
-          {player[key]}
-          {#if bonuses.player[key]}
-          <span class="combat-eval-bonus">({bonuses.player[key] > 0 ? '+' : ''}{bonuses.player[key]})</span>
-          {/if}
-        </td>
-      </tr>
-      {/each}
-    </tbody>
-  </table>
-  <table id="combat-eval-enemy-bonuses">
-    <thead><tr><th colspan="2">Monster Stats</th></tr></thead>
-    <tbody>
       <tr>
         <td>Will I be hit:</td>
-        <td>{willIBeHit ? 'Yes' : 'No'}</td>
+        {#if willIBeHit}
+        <td>Yes</td>
+        <td>(short {altVal(enemy.attack) - player.defense} defense)</td>
+        {:else}
+        <td>No</td>
+        <td>({player.defense - altVal(enemy.attack)} excess defense)</td>
+        {/if}
       </tr>
       <tr>
-        <td>#Hits to kill me:</td>
-        <td>{willIBeHit ? enemy.hits : '-'}</td>
+        <td># Hits to kill me:</td>
+        {#if willIBeHit}
+        <td>{enemy.hits > 20 ? ">20" : enemy.hits}</td>
+        <td>({Math.max(altVal(enemy.damage) - player.armor, 1)} damage per round)</td>
+        {:else}
+        <td>-</td>
+        {/if}
       </tr>
-      {#each statLabels as {label, key} (key)}
-      <tr>
-        <td>{label}:</td>
-        <td>
-          {enemy[key]}
-          {#if bonuses.enemy[key]}
-          <span class="combat-eval-bonus">({bonuses.enemy[key] > 0 ? '+' : ''}{bonuses.enemy[key]})</span>
-          {/if}
-        </td>
-      </tr>
-      {/each}
     </tbody>
   </table>
 </div>
 <p class="fshFooter">
-  *Accounts for {buffList()} and allow for randomness (1.1053).
+*Accounts for {buffList()} and allow for randomness ({calf.generalVariable}).
 </p>
 <style>
 #combat-eval-header {
   background: #cd9e4b;
   text-align: center;
 }
-#combat-eval-bonuses { columns: 2; }
 #combat-eval-bonuses table { width: 100%; }
 #combat-eval-bonuses table th { text-align: center; }
 .fshFooter, .combat-eval-bonus { color: gray; }
