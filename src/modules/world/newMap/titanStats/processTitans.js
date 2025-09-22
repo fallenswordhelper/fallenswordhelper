b308a5faa41c742c4a37208efe15db82684910bd
atompkins <atompkins@users.noreply.github.com>
2025-09-22 22:38:27 +0100

    fix-titan-stats

--- src/modules/world/newMap/titanStats/processTitans.js
+++ src/modules/world/newMap/titanStats/processTitans.js
@@ -1,7 +1,6 @@
 import textSpan from '../../../common/cElement/textSpan';
 import partial from '../../../common/partial';
 import roundToString from '../../../common/roundToString';
-import trimTitanName from '../../../common/trimTitanName';
 import setInnerHtml from '../../../dom/setInnerHtml';
 import setText from '../../../dom/setText';
 import getKillsPct from '../../../guild/scoutTower/getKillsPct';
@@ -11,7 +10,7 @@ import { now } from '../../../support/now';
 import padZ from '../../../system/padZ';
 import addRows from './addRows';
 import { clearMemberRows, getTitanTbl } from './buildTitanInfoTable';
-import { getTitanId, getTitanLoc } from './hasTitan';
+import { getTitanId, getTitanLoc, getTitanNm } from './hasTitan';
 import {
   getCooldownText,
   getCurrentHp,
@@ -58,7 +57,7 @@ function statusTextHtml(ourTitan) {
 
 function setAllText(ourTitan) {
   [
-    [trimTitanName(ourTitan.creature.name), getTitanName],
+    [getTitanNm(), getTitanName],
     [getTitanLoc(), getTitanLocation],
     [ourTitan.current_hp, getCurrentHp],
     [ourTitan.max_hp, getMaxHp],
@@ -102,7 +101,7 @@ function doMemberRows(ourTitan) {
 function currentTitan(el) {
   return (
     el.realm &&
-    el.creature.base_id === getTitanId() &&
+    el.creature === getTitanId() &&
     el.realm === getRealmName()
   );
 }
