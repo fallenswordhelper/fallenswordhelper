e31bd81b52f75d4056a6dd10dcd2e56145bd72ed
atompkins <atompkins@users.noreply.github.com>
2025-10-09 08:01:13 +0100

    updated-guild-tracker

--- src/modules/guild/stamBars/toggleStyle.js
+++ src/modules/guild/stamBars/toggleStyle.js
@@ -1,16 +1,43 @@
 import createStyle from '../../common/cElement/createStyle';
 import getElementsByTagName from '../../common/getElementsByTagName';
+import getText from '../../common/getText';
 import insertElement from '../../common/insertElement';
 import querySelectorArray from '../../common/querySelectorArray';
-import regExpGroups from '../../common/regExpGroups';
-import { defTable, playerLinkSelector, stamRe } from '../../support/constants';
+import { get } from '../../system/idb';
+import { defTable, playerLinkSelector } from '../../support/constants';
 import { pcc } from '../../support/layout';
+import {
+  cur,
+  fshGuildActivity,
+  max,
+} from '../../notepad/guildTracker/indexConstants';
 
 let thisStyle = 0;
+let trackerData = null;
+
+async function loadTrackerData() {
+  const raw = await get(fshGuildActivity);
+  if (raw?.members) {
+    trackerData = {};
+    for (const [name, history] of Object.entries(raw.members)) {
+      if (history.length > 0) {
+        const latest = history[history.length - 1];
+        trackerData[name] = {
+          current: latest[cur],
+          max: latest[max],
+        };
+      }
+    }
+  }
+}
 
 function getStamPerc(a) {
-  const { stam, max } = regExpGroups(stamRe, a.dataset.tipped);
-  return Math.min(Math.round((Number(stam) / Number(max)) * 100), 100);
+  const playerName = getText(a);
+  const data = trackerData?.[playerName];
+
+  if (!data) return 0;
+
+  return Math.min(Math.round((data.current / data.max) * 100), 100);
 }
 
 function stamBarStyle(a) {
@@ -24,21 +51,28 @@ function stamBarStyle(a) {
   );
 }
 
-function injectStyle() {
+async function injectStyle() {
+  await loadTrackerData();
+
   const tables = getElementsByTagName(defTable, pcc());
   const memberList = tables[tables.length - 1];
   const playerLinks = querySelectorArray(playerLinkSelector, memberList);
   if (!playerLinks.length) return;
-  const memberLinks = playerLinks.filter((a) => stamRe.test(a.dataset.tipped));
+
+  const memberLinks = playerLinks.filter((a) => {
+    const playerName = getText(a);
+    return trackerData?.[playerName];
+  });
+
   if (!memberLinks.length) return;
   memberList.classList.add('fshProgressBar');
   const myTest = memberLinks.map(stamBarStyle).join('\n');
   thisStyle = insertElement(document.body, createStyle(myTest)).sheet;
 }
 
-export default function toggleStyle(enableStamBars) {
+export default async function toggleStyle(enableStamBars) {
   if (!thisStyle) {
-    injectStyle();
+    await injectStyle();
   } else {
     thisStyle.disabled = !enableStamBars;
   }
