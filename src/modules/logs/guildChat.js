27b4bd99391ce7d62749a400526971323264edaf
atompkins <atompkins@users.noreply.github.com>
2025-11-15 18:01:19 +0000

    guild-chat

--- src/modules/logs/guildChat.js
+++ src/modules/logs/guildChat.js
@@ -1,15 +1,22 @@
+import './guildChat.css';
+import querySelector from '../common/querySelector';
 import getUrlParameter from '../system/getUrlParameter';
+import getValue from '../system/getValue';
 import addChatTextArea from './addChatTextArea';
 import addLogColoring from './addLogColoring';
-import guildChatStyling from './guildChatStyling';
+
+function guildChatStyling() {
+  const chatTable = querySelector('#pCC table table table table');
+  if (!chatTable) return;
+  const chatType = getUrlParameter('chat_type');
+  const chatStyle = chatType === '1' ? 'Leader' : 'Chat';
+  const cols = chatTable.rows[0].cells.length;
+  addLogColoring(chatStyle, 0, cols);
+  const wrapStyle = cols === 3 ? 'fsh-guild-chat' : 'fsh-guild-chat-action';
+  if (getValue('wrapGuildChat')) chatTable.classList.add(wrapStyle);
+}
 
 export default function guildChat() {
-  guildChatStyling();
   addChatTextArea();
-  const chatType = getUrlParameter('chat_type');
-  if (chatType && chatType === '1') {
-    addLogColoring('Leader', 0, 3);
-  } else {
-    addLogColoring('Chat', 0, 3);
-  }
+  guildChatStyling();
 }
