817e7e16e1e93c0e6f07c96ec6ac398d75be4068
Adrian Tompkins <atompkins@users.noreply.github.com>
2025-11-19 21:43:22 +0000

    drop-items (#843)

--- src/modules/profile/dropitems/injectProfileDropItems.js
+++ src/modules/profile/dropitems/injectProfileDropItems.js
@@ -1,13 +1,19 @@
+import { mount } from "svelte";
 import asyncPThree from '../../common/asyncPThree';
 import jQueryNotPresent from '../../common/jQueryNotPresent';
+import querySelector from "../../common/querySelector";
 import getCheckboxes from '../../guild/inventory/storeitems/getCheckboxes';
 import injectStoreItems from '../../guild/inventory/storeitems/injectStoreItems';
+import { pcc } from "../../support/layout";
+import DropItems from "./DropItems.svelte";
 import injectMoveItems from './injectMoveItems';
 import interceptDestroy from './interceptDestroy';
 
 const p3Functions = [injectMoveItems, injectStoreItems, interceptDestroy];
 
 export default function injectProfileDropItems() {
+  if (!querySelector('.backpack-manage-container')) return;
+  mount(DropItems, { target: pcc() });
   if (jQueryNotPresent() || !getCheckboxes()) {
     return;
   }
