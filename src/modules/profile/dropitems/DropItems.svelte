deac0cecd6c0573a67cbf889048972e151af412f
atompkins <atompkins@users.noreply.github.com>
2025-11-19 21:28:33 +0000

    drop-items

---
+++ src/modules/profile/dropitems/DropItems.svelte
@@ -0,0 +1,10 @@
+<script>
+  import sendEvent from "../../analytics/sendEvent";
+  import QuickSelectClasses from '../../common/QuickSelectClasses.svelte';
+</script>
+
+<QuickSelectClasses
+  dispatchPerf={() => sendEvent('drop-items', 'doPerf')}
+  dispatchSelect={(itemId) => sendEvent('drop-items', 'doSelect', itemId)}
+  dispatchToggle={() => sendEvent('drop-items', 'toggleSelectST')}
+/>
