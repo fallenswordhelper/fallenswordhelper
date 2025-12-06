943f2b95e4153d49b4b3d111c66cbaa393450d3b
Adrian Tompkins <atompkins@users.noreply.github.com>
2025-12-06 10:55:59 +0000

    xmas (#846)
    * xmas
    
    * add local time

---
+++ src/modules/points/xmas/xmas.js
@@ -0,0 +1,18 @@
+import formatLocalDateTime from '../../common/formatLocalDateTime';
+import formatUtcTimestamp from '../../common/formatUtcTimestamp';
+import insertHtmlBeforeEnd from '../../common/insertHtmlBeforeEnd';
+import { pcc } from '../../support/layout';
+
+/* global flamedeerData:true */
+
+export default function xmas() {
+  if (flamedeerData?.endTime && pcc()) {
+    insertHtmlBeforeEnd(
+      pcc(),
+      `<div style="color: blue; text-align: center;">
+        Ends at: ${formatUtcTimestamp(flamedeerData.endTime)}
+        (Local: ${formatLocalDateTime(new Date(flamedeerData.endTime * 1000))})
+      </div>`,
+    );
+  }
+}
